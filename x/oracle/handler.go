325d45c325673ac7f4d4e4b4e862a2b1a72d62ad
Jack Zampolin <jack.zampolin@gmail.com>
2020-12-08 17:39:48 -0800

    remove unnecessary annotations

--- x/oracle/handler.go
+++ x/oracle/handler.go
@@ -12,10 +12,8 @@ import (
 // NewHandler returns a handler for "oracle" type messages.
 func NewHandler(k keeper.Keeper) sdk.Handler {
 	msgServer := keeper.NewMsgServerImpl(k)
-
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
-
 		switch msg := msg.(type) {
 		case *types.MsgDelegateFeedConsent:
 			res, err := msgServer.DelegateFeedConsent(sdk.WrapSDKContext(ctx), msg)
