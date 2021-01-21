3e9b033684268f4d71512256c9253496a12d0722
Federico Kunze <federico.kunze94@gmail.com>
2021-01-21 17:59:24 -0300

    x/il: impermanent loss module

--- go.mod
+++ go.mod
@@ -3,33 +3,22 @@ module github.com/peggyjv/sommelier
 go 1.15
 
 require (
-	github.com/Masterminds/goutils v1.1.0 // indirect
-	github.com/Masterminds/semver v1.5.0 // indirect
-	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
-	github.com/aokoli/goutils v1.1.0 // indirect
 	github.com/cosmos/cosmos-sdk v0.40.0
-	github.com/envoyproxy/protoc-gen-validate v0.4.1 // indirect
-	github.com/ethereum/go-ethereum v1.9.25
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/protobuf v1.4.3
+	github.com/golang/snappy v0.0.3-0.20201103224600-674baa8c7fc3 // indirect
 	github.com/google/gofuzz v1.1.1-0.20200604201612-c04b05f3adfa // indirect
-	github.com/google/uuid v1.1.4 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
-	github.com/huandu/xstrings v1.3.2 // indirect
-	github.com/imdario/mergo v0.3.11 // indirect
-	github.com/mitchellh/copystructure v1.0.0 // indirect
-	github.com/mitchellh/reflectwalk v1.0.1 // indirect
-	github.com/mwitkow/go-proto-validators v0.3.2 // indirect
 	github.com/pkg/errors v0.9.1
-	github.com/pseudomuto/protoc-gen-doc v1.3.2 // indirect
 	github.com/rakyll/statik v0.1.7
+	github.com/regen-network/cosmos-proto v0.3.1 // indirect
 	github.com/spf13/cast v1.3.1
 	github.com/spf13/cobra v1.1.1
 	github.com/stretchr/testify v1.6.1
 	github.com/tendermint/tendermint v0.34.1
 	github.com/tendermint/tm-db v0.6.3
-	google.golang.org/genproto v0.0.0-20210111173611-c7d5778d165c
+	google.golang.org/genproto v0.0.0-20210121164019-fc48d45331c7
 	google.golang.org/grpc v1.33.2
 	gopkg.in/yaml.v2 v2.4.0
 )
--- x/il/keeper/grpc_query.go
+++ x/il/keeper/grpc_query.go
@@ -1,243 +1,243 @@
 package keeper
 
 import (
-	"context"
+	// "context"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	// "google.golang.org/grpc/codes"
+	// "google.golang.org/grpc/status"
 
-	"github.com/cosmos/cosmos-sdk/store/prefix"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/cosmos/cosmos-sdk/types/query"
+	// "github.com/cosmos/cosmos-sdk/store/prefix"
+	// sdk "github.com/cosmos/cosmos-sdk/types"
+	// sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	// "github.com/cosmos/cosmos-sdk/types/query"
 
-	"github.com/peggyjv/sommelier/x/oracle/types"
+	"github.com/peggyjv/sommelier/x/il/types"
 )
 
 var _ types.QueryServer = Keeper{}
 
-// ExchangeRate returns the current exchange rate for a given denom
-func (k Keeper) ExchangeRate(c context.Context, req *types.QueryExchangeRateRequest) (*types.QueryExchangeRateResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // ExchangeRate returns the current exchange rate for a given denom
+// func (k Keeper) ExchangeRate(c context.Context, req *types.QueryExchangeRateRequest) (*types.QueryExchangeRateResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	rate, err := k.GetUSDExchangeRate(sdk.UnwrapSDKContext(c), req.Denom)
-	if err != nil {
-		return nil, sdkerrors.Wrap(types.ErrUnknowDenom, req.Denom)
-	}
+// 	rate, err := k.GetUSDExchangeRate(sdk.UnwrapSDKContext(c), req.Denom)
+// 	if err != nil {
+// 		return nil, sdkerrors.Wrap(types.ErrUnknowDenom, req.Denom)
+// 	}
 
-	return &types.QueryExchangeRateResponse{Rate: rate}, nil
-}
+// 	return &types.QueryExchangeRateResponse{Rate: rate}, nil
+// }
 
-// ExchangeRates returns all the exchange rates tracked by the system
-func (k Keeper) ExchangeRates(c context.Context, req *types.QueryExchangeRatesRequest) (*types.QueryExchangeRatesResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // ExchangeRates returns all the exchange rates tracked by the system
+// func (k Keeper) ExchangeRates(c context.Context, req *types.QueryExchangeRatesRequest) (*types.QueryExchangeRatesResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	ctx := sdk.UnwrapSDKContext(c)
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.ExchangeRateKey)
+// 	ctx := sdk.UnwrapSDKContext(c)
+// 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.ExchangeRateKey)
 
-	rates := sdk.DecCoins{}
-	pageRes, err := query.Paginate(store, req.Pagination, func(key, value []byte) error {
-		var result sdk.Dec
-		err := result.Unmarshal(value)
-		if err != nil {
-			return err
-		}
+// 	rates := sdk.DecCoins{}
+// 	pageRes, err := query.Paginate(store, req.Pagination, func(key, value []byte) error {
+// 		var result sdk.Dec
+// 		err := result.Unmarshal(value)
+// 		if err != nil {
+// 			return err
+// 		}
 
-		denom := string(key)
-		rate := sdk.NewDecCoinFromDec(denom, result)
+// 		denom := string(key)
+// 		rate := sdk.NewDecCoinFromDec(denom, result)
 
-		rates = rates.Add(rate)
-		return nil
-	})
+// 		rates = rates.Add(rate)
+// 		return nil
+// 	})
 
-	if err != nil {
-		return nil, err
-	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return &types.QueryExchangeRatesResponse{
-		Rates:      rates,
-		Pagination: pageRes,
-	}, nil
-}
+// 	return &types.QueryExchangeRatesResponse{
+// 		Rates:      rates,
+// 		Pagination: pageRes,
+// 	}, nil
+// }
 
-// Actives returns the active denoms
-func (k Keeper) Actives(c context.Context, req *types.QueryActivesRequest) (*types.QueryActivesResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // Actives returns the active denoms
+// func (k Keeper) Actives(c context.Context, req *types.QueryActivesRequest) (*types.QueryActivesResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	ctx := sdk.UnwrapSDKContext(c)
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.ExchangeRateKey)
+// 	ctx := sdk.UnwrapSDKContext(c)
+// 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.ExchangeRateKey)
 
-	actives := []string{}
-	pageRes, err := query.Paginate(store, req.Pagination, func(key, _ []byte) error {
-		actives = append(actives, string(key))
-		return nil
-	})
+// 	actives := []string{}
+// 	pageRes, err := query.Paginate(store, req.Pagination, func(key, _ []byte) error {
+// 		actives = append(actives, string(key))
+// 		return nil
+// 	})
 
-	if err != nil {
-		return nil, err
-	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return &types.QueryActivesResponse{
-		Denoms:     actives,
-		Pagination: pageRes,
-	}, nil
-}
+// 	return &types.QueryActivesResponse{
+// 		Denoms:     actives,
+// 		Pagination: pageRes,
+// 	}, nil
+// }
 
-// Parameters returns the oracle module parameters
-func (k Keeper) Parameters(c context.Context, _ *types.QueryParametersRequest) (*types.QueryParametersResponse, error) {
-	return &types.QueryParametersResponse{Params: k.GetParams(sdk.UnwrapSDKContext(c))}, nil
-}
+// // Parameters returns the oracle module parameters
+// func (k Keeper) Parameters(c context.Context, _ *types.QueryParametersRequest) (*types.QueryParametersResponse, error) {
+// 	return &types.QueryParametersResponse{Params: k.GetParams(sdk.UnwrapSDKContext(c))}, nil
+// }
 
-// FeederDelegation returns the address to which a validator is delegating the feeder responsibility
-func (k Keeper) FeederDelegation(c context.Context, req *types.QueryFeederDelegationRequest) (*types.QueryFeederDelegationResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // FeederDelegation returns the address to which a validator is delegating the feeder responsibility
+// func (k Keeper) FeederDelegation(c context.Context, req *types.QueryFeederDelegationRequest) (*types.QueryFeederDelegationResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	addr, err := sdk.ValAddressFromBech32(req.Validator)
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
-	}
+// 	addr, err := sdk.ValAddressFromBech32(req.Validator)
+// 	if err != nil {
+// 		return nil, status.Error(codes.InvalidArgument, err.Error())
+// 	}
 
-	return &types.QueryFeederDelegationResponse{
-		Address: k.GetOracleDelegate(sdk.UnwrapSDKContext(c), addr).String(),
-	}, nil
-}
+// 	return &types.QueryFeederDelegationResponse{
+// 		Address: k.GetOracleDelegate(sdk.UnwrapSDKContext(c), addr).String(),
+// 	}, nil
+// }
 
-// MissCounter returns the number of misses for a given validator
-func (k Keeper) MissCounter(c context.Context, req *types.QueryMissCounterRequest) (*types.QueryMissCounterResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // MissCounter returns the number of misses for a given validator
+// func (k Keeper) MissCounter(c context.Context, req *types.QueryMissCounterRequest) (*types.QueryMissCounterResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	addr, err := sdk.ValAddressFromBech32(req.Validator)
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
-	}
+// 	addr, err := sdk.ValAddressFromBech32(req.Validator)
+// 	if err != nil {
+// 		return nil, status.Error(codes.InvalidArgument, err.Error())
+// 	}
 
-	ctx := sdk.UnwrapSDKContext(c)
+// 	ctx := sdk.UnwrapSDKContext(c)
 
-	counter, found := k.GetMissCounter(ctx, addr)
-	if !found {
-		return nil, status.Error(codes.NotFound, req.Validator)
-	}
+// 	counter, found := k.GetMissCounter(ctx, addr)
+// 	if !found {
+// 		return nil, status.Error(codes.NotFound, req.Validator)
+// 	}
 
-	return &types.QueryMissCounterResponse{Counter: counter}, nil
-}
+// 	return &types.QueryMissCounterResponse{Counter: counter}, nil
+// }
 
-// AggregatePrevote returns the latest aggregate prevote from a given validator
-func (k Keeper) AggregatePrevote(c context.Context, req *types.QueryAggregatePrevoteRequest) (*types.QueryAggregatePrevoteResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // AggregatePrevote returns the latest aggregate prevote from a given validator
+// func (k Keeper) AggregatePrevote(c context.Context, req *types.QueryAggregatePrevoteRequest) (*types.QueryAggregatePrevoteResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	addr, err := sdk.ValAddressFromBech32(req.Validator)
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
-	}
+// 	addr, err := sdk.ValAddressFromBech32(req.Validator)
+// 	if err != nil {
+// 		return nil, status.Error(codes.InvalidArgument, err.Error())
+// 	}
 
-	prevote, err := k.GetAggregateExchangeRatePrevote(sdk.UnwrapSDKContext(c), addr)
-	if err != nil {
-		return nil, status.Error(codes.NotFound, err.Error())
-	}
+// 	prevote, err := k.GetAggregateExchangeRatePrevote(sdk.UnwrapSDKContext(c), addr)
+// 	if err != nil {
+// 		return nil, status.Error(codes.NotFound, err.Error())
+// 	}
 
-	return &types.QueryAggregatePrevoteResponse{Prevote: prevote}, nil
-}
+// 	return &types.QueryAggregatePrevoteResponse{Prevote: prevote}, nil
+// }
 
-// AggregateVote returns the latest aggregate prevote from a given validator
-func (k Keeper) AggregateVote(c context.Context, req *types.QueryAggregateVoteRequest) (*types.QueryAggregateVoteResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // AggregateVote returns the latest aggregate prevote from a given validator
+// func (k Keeper) AggregateVote(c context.Context, req *types.QueryAggregateVoteRequest) (*types.QueryAggregateVoteResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	addr, err := sdk.ValAddressFromBech32(req.Validator)
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
-	}
+// 	addr, err := sdk.ValAddressFromBech32(req.Validator)
+// 	if err != nil {
+// 		return nil, status.Error(codes.InvalidArgument, err.Error())
+// 	}
 
-	vote, err := k.GetAggregateExchangeRateVote(sdk.UnwrapSDKContext(c), addr)
-	if err != nil {
-		return nil, status.Error(codes.NotFound, err.Error())
-	}
+// 	vote, err := k.GetAggregateExchangeRateVote(sdk.UnwrapSDKContext(c), addr)
+// 	if err != nil {
+// 		return nil, status.Error(codes.NotFound, err.Error())
+// 	}
 
-	return &types.QueryAggregateVoteResponse{Vote: vote}, nil
-}
+// 	return &types.QueryAggregateVoteResponse{Vote: vote}, nil
+// }
 
-// VoteTargets returns the target denoms for voting?
-func (k Keeper) VoteTargets(c context.Context, req *types.QueryVoteTargetsRequest) (*types.QueryVoteTargetsResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // VoteTargets returns the target denoms for voting?
+// func (k Keeper) VoteTargets(c context.Context, req *types.QueryVoteTargetsRequest) (*types.QueryVoteTargetsResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	ctx := sdk.UnwrapSDKContext(c)
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.TobinTaxKey)
+// 	ctx := sdk.UnwrapSDKContext(c)
+// 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.TobinTaxKey)
 
-	targets := []string{}
-	pageRes, err := query.Paginate(store, req.Pagination, func(key, _ []byte) error {
-		denom := string(key)
-		targets = append(targets, denom)
-		return nil
-	})
+// 	targets := []string{}
+// 	pageRes, err := query.Paginate(store, req.Pagination, func(key, _ []byte) error {
+// 		denom := string(key)
+// 		targets = append(targets, denom)
+// 		return nil
+// 	})
 
-	if err != nil {
-		return nil, err
-	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return &types.QueryVoteTargetsResponse{
-		Targets:    targets,
-		Pagination: pageRes,
-	}, nil
-}
+// 	return &types.QueryVoteTargetsResponse{
+// 		Targets:    targets,
+// 		Pagination: pageRes,
+// 	}, nil
+// }
 
-// TobinTax returns the current tobin tax given a denom
-func (k Keeper) TobinTax(c context.Context, req *types.QueryTobinTaxRequest) (*types.QueryTobinTaxResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // TobinTax returns the current tobin tax given a denom
+// func (k Keeper) TobinTax(c context.Context, req *types.QueryTobinTaxRequest) (*types.QueryTobinTaxResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	tobinTax, err := k.GetTobinTax(sdk.UnwrapSDKContext(c), req.Denom)
-	if err != nil {
-		return nil, status.Error(codes.NotFound, err.Error())
-	}
+// 	tobinTax, err := k.GetTobinTax(sdk.UnwrapSDKContext(c), req.Denom)
+// 	if err != nil {
+// 		return nil, status.Error(codes.NotFound, err.Error())
+// 	}
 
-	return &types.QueryTobinTaxResponse{Rate: tobinTax}, nil
-}
+// 	return &types.QueryTobinTaxResponse{Rate: tobinTax}, nil
+// }
 
-// TobinTaxes returns all the tobin taxes tracked by the system
-func (k Keeper) TobinTaxes(c context.Context, req *types.QueryTobinTaxesRequest) (*types.QueryTobinTaxesResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "empty request")
-	}
+// // TobinTaxes returns all the tobin taxes tracked by the system
+// func (k Keeper) TobinTaxes(c context.Context, req *types.QueryTobinTaxesRequest) (*types.QueryTobinTaxesResponse, error) {
+// 	if req == nil {
+// 		return nil, status.Error(codes.InvalidArgument, "empty request")
+// 	}
 
-	ctx := sdk.UnwrapSDKContext(c)
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.TobinTaxKey)
+// 	ctx := sdk.UnwrapSDKContext(c)
+// 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.TobinTaxKey)
 
-	taxes := sdk.DecCoins{}
-	pageRes, err := query.Paginate(store, req.Pagination, func(key, value []byte) error {
-		var result sdk.Dec
-		err := result.Unmarshal(value)
-		if err != nil {
-			return err
-		}
+// 	taxes := sdk.DecCoins{}
+// 	pageRes, err := query.Paginate(store, req.Pagination, func(key, value []byte) error {
+// 		var result sdk.Dec
+// 		err := result.Unmarshal(value)
+// 		if err != nil {
+// 			return err
+// 		}
 
-		denom := string(key)
-		rate := sdk.NewDecCoinFromDec(denom, result)
+// 		denom := string(key)
+// 		rate := sdk.NewDecCoinFromDec(denom, result)
 
-		taxes = taxes.Add(rate)
-		return nil
-	})
+// 		taxes = taxes.Add(rate)
+// 		return nil
+// 	})
 
-	if err != nil {
-		return nil, err
-	}
-	return &types.QueryTobinTaxesResponse{
-		Rates:      taxes,
-		Pagination: pageRes,
-	}, nil
-}
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &types.QueryTobinTaxesResponse{
+// 		Rates:      taxes,
+// 		Pagination: pageRes,
+// 	}, nil
+// }
