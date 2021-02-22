956f329ada0549ba928ed10dc2fa11cbdbed351b
Federico Kunze <federico.kunze94@gmail.com>
2021-02-22 17:46:29 -0300

    gen validation

--- x/oracle/types/genesis.go
+++ x/oracle/types/genesis.go
@@ -6,36 +6,54 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// NewGenesisState returns the genesis state struct
-func NewGenesisState() *GenesisState {
-	return &GenesisState{}
-}
-
 // DefaultGenesisState get raw genesis raw message for testing
-func DefaultGenesisState() *GenesisState {
-	return &GenesisState{
+func DefaultGenesisState() GenesisState {
+	return GenesisState{
 		Params: DefaultParams(),
 	}
 }
 
 // Validate performs a basic stateless validation of the genesis fields.
 func (gs GenesisState) Validate() error {
+	seenDelegations := make(map[string]bool)
+	seenMissCounters := make(map[string]bool)
+
 	for i, feederDelegation := range gs.FeederDelegations {
-		if _, err := sdk.AccAddressFromBech32(feederDelegation.Delegate); err != nil {
-			return fmt.Errorf("invalid feeder at index %d: %w", i, err)
+		if seenDelegations[feederDelegation.Validator] {
+			return fmt.Errorf("duplicated feeder delegation for validator %s at index %d", feederDelegation.Validator, i)
 		}
-		if _, err := sdk.AccAddressFromBech32(feederDelegation.Validator); err != nil {
-			return fmt.Errorf("invalid feeder at index %d: %w", i, err)
+
+		delegateAddr, err := sdk.AccAddressFromBech32(feederDelegation.Delegate)
+		if err != nil {
+			return fmt.Errorf("invalid feeder delegate at index %d: %w", i, err)
 		}
+
+		validatorAddr, err := sdk.ValAddressFromBech32(feederDelegation.Validator)
+		if err != nil {
+			return fmt.Errorf("invalid feeder validator at index %d: %w", i, err)
+		}
+
+		if delegateAddr.Equals(validatorAddr) {
+			return fmt.Errorf("delegate address %s cannot be equal to validator address %s", feederDelegation.Delegate, feederDelegation.Validator)
+		}
+
+		seenDelegations[feederDelegation.Validator] = true
 	}
 
 	for i, missCounter := range gs.MissCounters {
+		if seenMissCounters[missCounter.Validator] {
+			return fmt.Errorf("duplicated miss counter for validator %s at index %d", missCounter.Validator, i)
+		}
+
 		if missCounter.Misses < 0 {
 			return fmt.Errorf("miss counter for validator %s cannot be negative: %d", missCounter.Validator, missCounter.Misses)
 		}
-		if _, err := sdk.AccAddressFromBech32(missCounter.Validator); err != nil {
+
+		if _, err := sdk.ValAddressFromBech32(missCounter.Validator); err != nil {
 			return fmt.Errorf("invalid feeder at index %d: %w", i, err)
 		}
+
+		seenMissCounters[missCounter.Validator] = true
 	}
 
 	return gs.Params.ValidateBasic()
