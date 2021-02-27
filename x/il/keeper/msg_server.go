cc54456918e6118e09a7f58d4ffe8b0796e066d8
Federico Kunze <federico.kunze94@gmail.com>
2021-02-27 09:37:41 -0300

    events and metrics

--- x/il/keeper/msg_server.go
+++ x/il/keeper/msg_server.go
@@ -3,6 +3,8 @@ package keeper
 import (
 	"context"
 
+	"github.com/armon/go-metrics"
+	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
@@ -21,6 +23,8 @@ func (k Keeper) CreateStoploss(c context.Context, msg *types.MsgStoploss) (*type
 	address, _ := sdk.AccAddressFromBech32(msg.Address)
 
 	// TODO: check if uniswap pair exists on the oracle
+	// oracleData, found := k.oracleKeeper.GetLatestAggregatedOracleData(ctx, oracletypes.UniswapDataType ,msg.Stoploss.UniswapPairId)
+
 	// TODO: ensure that pair ratio is w/in band of pair ratio from oracle
 
 	// check if there's already a position for that pair
@@ -28,7 +32,6 @@ func (k Keeper) CreateStoploss(c context.Context, msg *types.MsgStoploss) (*type
 		return nil, sdkerrors.Wrapf(types.ErrStoplossExists, "address: %s, uniswap pair id %s", address, msg.Stoploss.UniswapPairId)
 	}
 
-	// Set the delegation
 	k.SetStoplossPosition(ctx, address, *msg.Stoploss)
 
 	ctx.EventManager().EmitEvents(sdk.Events{
@@ -42,6 +45,16 @@ func (k Keeper) CreateStoploss(c context.Context, msg *types.MsgStoploss) (*type
 			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
 		),
 	})
+
+	defer func() {
+		telemetry.IncrCounterWithLabels(
+			[]string{types.ModuleName, "create-stoploss"},
+			1,
+			[]metrics.Label{
+				{Name: "pair-id", Value: msg.Stoploss.UniswapPairId},
+			},
+		)
+	}()
 
 	return &types.MsgStoplossResponse{}, nil
 }
@@ -58,7 +71,7 @@ func (k Keeper) DeleteStoploss(c context.Context, msg *types.MsgDeleteStoploss) 
 		return nil, sdkerrors.Wrapf(types.ErrStoplossNotFound, "address: %s, uniswap pair id %s", address, msg.UniswapPairId)
 	}
 
-	// Set the delegation
+	// delete the stoploss position
 	k.DeleteStoplossPosition(ctx, address, msg.UniswapPairId)
 
 	ctx.EventManager().EmitEvents(sdk.Events{
@@ -72,6 +85,16 @@ func (k Keeper) DeleteStoploss(c context.Context, msg *types.MsgDeleteStoploss) 
 			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
 		),
 	})
+
+	defer func() {
+		telemetry.IncrCounterWithLabels(
+			[]string{types.ModuleName, "delete-stoploss"},
+			1,
+			[]metrics.Label{
+				telemetry.NewLabel("pair-id", msg.UniswapPairId),
+			},
+		)
+	}()
 
 	return &types.MsgDeleteStoplossResponse{}, nil
 }
