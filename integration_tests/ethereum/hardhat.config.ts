fdc668c4328a1f9f1f0a047d007765e7446e13b0
Kevin Kennis <kevinvkennis@gmail.com>
2021-09-08 12:57:57 -0700

    fund accounts with eth

--- integration_tests/ethereum/hardhat.config.ts
+++ integration_tests/ethereum/hardhat.config.ts
@@ -6,7 +6,8 @@ task('integration_test_setup', 'Sets up contracts for the integration test', asy
   const ADDRESSES = {
     CELLAR_OWNER: '0xB6C951cf962977f123bF37de42945f7ca1cd2A52',
     CELLAR: '0x6ea5992aB4A78D5720bD12A089D13c073d04B55d',
-    GRAVITY: '0xFbB0BCfed0c82043A7d5387C35Ad8450b44A4cde'
+    GRAVITY: '0xFbB0BCfed0c82043A7d5387C35Ad8450b44A4cde',
+    WHALE: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045'
   };
 
   // Take over the cellar owner so we can transfer ownership
@@ -15,7 +16,7 @@ task('integration_test_setup', 'Sets up contracts for the integration test', asy
     params: [ADDRESSES.CELLAR_OWNER]
   });
 
-  const signer = await hre.ethers.getSigner(ADDRESSES.CELLAR_OWNER)
+  const signer = await hre.ethers.getSigner(ADDRESSES.CELLAR_OWNER);
 
   // Transfer ownership to gravity
   const Cellar = new hre.ethers.ContractFactory(CellarArtifact.abi, CellarArtifact.bytecode, signer);
@@ -23,11 +24,36 @@ task('integration_test_setup', 'Sets up contracts for the integration test', asy
 
   const { hash } = await cellar.transferOwnership(ADDRESSES.GRAVITY);
 
+  // Send ETH to needed accounts
+
   console.log(`Cellar contract at ${ADDRESSES.CELLAR} is now owned by Gravity contract at ${ADDRESSES.GRAVITY}`);
   console.log(`Tx hash: ${hash}`);
   console.log('='.repeat(80));
 
   await hre.run('node');
+
+  // Take over vitalik.eth
+  await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [ADDRESSES.WHALE]
+  });
+
+  // Send ETH to needed parties
+  const whaleSigner = await hre.ethers.getSigner(ADDRESSES.WHALE);
+
+  const recipients = [
+    '0xd312f0f1B39D54Db2829537595fC1167B14d4b34',
+    '0x7bE2a04df4b9C3227928147461e19158eB2B11d1',
+    '0xb8c6886FDDa38adaa0F416722dd5554886C43055',
+    '0x14fdAC734De10065093C4Ed4a83C41638378005A'
+  ];
+
+  for (let addr of recipients) {
+    await whaleSigner.sendTransaction({
+      to: addr,
+      value: hre.ethers.utils.parseEther('100')
+    });
+  }
 });
 
 /**
