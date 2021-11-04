d40270c99fc4e0ef3761a1b311d5c5381ee18621
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-11-04 15:57:39 +0000

    docker running ganache/brownie

--- integration_tests/brownie/startup.sh
+++ integration_tests/brownie/startup.sh
@@ -1,2 +1,7 @@
-ganache-cli --fork https://mainnet.infura.io/v3/d6f22be0f7fd447186086d2495779003 -p 7545
-pipenv run brownie testnet --network=local
+echo "starting ganache"
+ganache-cli --fork https://mainnet.infura.io/v3/d6f22be0f7fd447186086d2495779003 -p 8545 &
+
+echo "running brownie script"
+pipenv run brownie run ./scripts/testnet.py --network=development
+
+echo "brownie complete"
