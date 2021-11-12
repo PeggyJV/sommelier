b37ce3f036154d04a3168a97d58bc76d38deb203
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-11-12 18:40:15 -0400

    successful deployment

--- integration_tests/setup_test.go
+++ integration_tests/setup_test.go
@@ -60,7 +60,7 @@ func MNEMONICS() []string {
 var (
 	stakeAmount, _  = sdk.NewIntFromString("100000000000")
 	stakeAmountCoin = sdk.NewCoin(bondDenom, stakeAmount)
-	hardhatCellar   = common.HexToAddress("0x08c0a0B8D2eDB1d040d4f2C00A1d2f9d9b9F2677")
+	hardhatCellar   = common.HexToAddress("0x4C4a2f8c81640e47606d3fd77B353E87Ba015584")
 	gravityContract = common.HexToAddress("0x04C89607413713Ec9775E14b954286519d836FEf")
 )
 
