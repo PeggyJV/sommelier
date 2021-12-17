b11835f56bbdd64f46d6b37683459cadc24f2b75
Zaki Manian <zaki@iqlusion.io>
2021-12-16 19:12:44 -0800

    Need two solc compilers

--- integration_tests/ethereum/hardhat.config.ts
+++ integration_tests/ethereum/hardhat.config.ts
@@ -93,11 +93,19 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: '0.8.10',
+                version: '0.7.6',
                 settings: {
                     optimizer: {
                         enabled: true,
                         runs: 200,
+                    },
+                },
+            },
+            {
+                version: '0.8.10',
+                settings: {
+                    optimizer: {
+                        enabled: true,
                     },
                 },
             },
