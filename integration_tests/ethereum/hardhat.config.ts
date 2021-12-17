81b6676441f5533f62bbdef4c7a20a3a429b6fe9
Zaki Manian <zaki@iqlusion.io>
2021-12-16 19:03:56 -0800

    Sync solidity dependencies

--- integration_tests/ethereum/hardhat.config.ts
+++ integration_tests/ethereum/hardhat.config.ts
@@ -93,28 +93,11 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: '0.6.6',
+                version: '0.8.10',
                 settings: {
                     optimizer: {
                         enabled: true,
                         runs: 200,
-                    },
-                },
-            },
-            {
-                version: '0.7.6',
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 200,
-                    },
-                },
-            },
-            {
-                version: '0.8.0',
-                settings: {
-                    optimizer: {
-                        enabled: true,
                     },
                 },
             },
