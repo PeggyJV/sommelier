f5d7c5db4738db9414e05b7a9fa52146c6316dfc
Eric Bolten <eric@bolten.dev>
2022-02-17 22:25:39 -0800

    Update genesis proto

--- x/pubsub/types/genesis.pb.go
+++ x/pubsub/types/genesis.pb.go
@@ -25,7 +25,11 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // GenesisState defines the pubsub module's genesis state.
 type GenesisState struct {
-	Params Params `protobuf:"bytes,1,opt,name=params,proto3" json:"params"`
+	Params            Params              `protobuf:"bytes,1,opt,name=params,proto3" json:"params"`
+	Publishers        []*Publisher        `protobuf:"bytes,2,rep,name=publishers,proto3" json:"publishers,omitempty"`
+	Subscribers       []*Subscriber       `protobuf:"bytes,3,rep,name=subscribers,proto3" json:"subscribers,omitempty"`
+	PublisherIntents  []*PublisherIntent  `protobuf:"bytes,4,rep,name=publisher_intents,json=publisherIntents,proto3" json:"publisher_intents,omitempty"`
+	SubscriberIntents []*SubscriberIntent `protobuf:"bytes,5,rep,name=subscriber_intents,json=subscriberIntents,proto3" json:"subscriber_intents,omitempty"`
 }
 
 func (m *GenesisState) Reset()         { *m = GenesisState{} }
@@ -68,6 +72,34 @@ func (m *GenesisState) GetParams() Params {
 	return Params{}
 }
 
+func (m *GenesisState) GetPublishers() []*Publisher {
+	if m != nil {
+		return m.Publishers
+	}
+	return nil
+}
+
+func (m *GenesisState) GetSubscribers() []*Subscriber {
+	if m != nil {
+		return m.Subscribers
+	}
+	return nil
+}
+
+func (m *GenesisState) GetPublisherIntents() []*PublisherIntent {
+	if m != nil {
+		return m.PublisherIntents
+	}
+	return nil
+}
+
+func (m *GenesisState) GetSubscriberIntents() []*SubscriberIntent {
+	if m != nil {
+		return m.SubscriberIntents
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*GenesisState)(nil), "pubsub.v1.GenesisState")
 }
@@ -75,20 +107,27 @@ func init() {
 func init() { proto.RegisterFile("pubsub/v1/genesis.proto", fileDescriptor_0c358cacca624823) }
 
 var fileDescriptor_0c358cacca624823 = []byte{
-	// 198 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2f, 0x28, 0x4d, 0x2a,
-	0x2e, 0x4d, 0xd2, 0x2f, 0x33, 0xd4, 0x4f, 0x4f, 0xcd, 0x4b, 0x2d, 0xce, 0x2c, 0xd6, 0x2b, 0x28,
-	0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x84, 0x48, 0xe8, 0x95, 0x19, 0x4a, 0x89, 0xa4, 0xe7, 0xa7, 0xe7,
-	0x83, 0x45, 0xf5, 0x41, 0x2c, 0x88, 0x02, 0x29, 0x31, 0x84, 0xce, 0x82, 0xc4, 0xa2, 0xc4, 0x5c,
-	0xa8, 0x46, 0x25, 0x7b, 0x2e, 0x1e, 0x77, 0x88, 0x49, 0xc1, 0x25, 0x89, 0x25, 0xa9, 0x42, 0xfa,
-	0x5c, 0x6c, 0x10, 0x79, 0x09, 0x46, 0x05, 0x46, 0x0d, 0x6e, 0x23, 0x41, 0x3d, 0xb8, 0xc9, 0x7a,
-	0x01, 0x60, 0x09, 0x27, 0x96, 0x13, 0xf7, 0xe4, 0x19, 0x82, 0xa0, 0xca, 0x9c, 0x3c, 0x4e, 0x3c,
-	0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e,
-	0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x21, 0x4a, 0x2f, 0x3d, 0xb3, 0x24, 0xa3, 0x34, 0x49,
-	0x2f, 0x39, 0x3f, 0x57, 0xbf, 0x20, 0x35, 0x3d, 0xbd, 0x32, 0xab, 0x4c, 0xbf, 0x38, 0x3f, 0x37,
-	0x37, 0x35, 0x27, 0x33, 0xb5, 0x48, 0xbf, 0xcc, 0x58, 0xbf, 0x42, 0x1f, 0xea, 0xa8, 0x92, 0xca,
-	0x82, 0xd4, 0xe2, 0x24, 0x36, 0xb0, 0x8b, 0x8c, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x54, 0xdc,
-	0x6c, 0xd0, 0xe5, 0x00, 0x00, 0x00,
+	// 309 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x91, 0xb1, 0x4f, 0x3a, 0x31,
+	0x1c, 0xc5, 0xef, 0x80, 0x1f, 0xc9, 0xaf, 0x38, 0x48, 0x83, 0x4a, 0x30, 0xa9, 0xc4, 0x89, 0xa9,
+	0x0d, 0x60, 0xe2, 0xce, 0x82, 0x3a, 0x99, 0x63, 0x73, 0x31, 0x94, 0x34, 0xa5, 0x86, 0xa3, 0xcd,
+	0x7d, 0x7b, 0x17, 0xf9, 0x2f, 0xfc, 0x9f, 0x5c, 0x18, 0x19, 0x9d, 0x8c, 0xe1, 0xfe, 0x11, 0x43,
+	0xef, 0x3c, 0x4e, 0x73, 0x5b, 0xf3, 0xde, 0xfb, 0xbc, 0xd7, 0xe4, 0x8b, 0x2e, 0x4c, 0xcc, 0x21,
+	0xe6, 0x2c, 0x19, 0x32, 0x29, 0xd6, 0x02, 0x14, 0x50, 0x13, 0x69, 0xab, 0xf1, 0xff, 0xcc, 0xa0,
+	0xc9, 0xb0, 0xd7, 0x91, 0x5a, 0x6a, 0xa7, 0xb2, 0xc3, 0x2b, 0x0b, 0xf4, 0xce, 0x8f, 0xa4, 0x99,
+	0x47, 0xf3, 0x10, 0x2a, 0xf4, 0xac, 0xc2, 0xe9, 0xd7, 0xef, 0x35, 0x74, 0x32, 0xcd, 0x26, 0x66,
+	0x76, 0x6e, 0x05, 0x66, 0xa8, 0x99, 0x81, 0x5d, 0xbf, 0xef, 0x0f, 0x5a, 0xa3, 0x36, 0x2d, 0x26,
+	0xe9, 0xa3, 0x33, 0x26, 0x8d, 0xed, 0xe7, 0x95, 0x17, 0xe4, 0x31, 0x7c, 0x83, 0x90, 0x89, 0xf9,
+	0x4a, 0xc1, 0x52, 0x44, 0xd0, 0xad, 0xf5, 0xeb, 0x83, 0xd6, 0xa8, 0x53, 0x86, 0x7e, 0xcc, 0xa0,
+	0x94, 0xc3, 0xb7, 0xa8, 0x05, 0x31, 0x87, 0x45, 0xa4, 0xf8, 0x01, 0xab, 0x3b, 0xec, 0xac, 0x84,
+	0xcd, 0x0a, 0x37, 0x28, 0x27, 0xf1, 0x14, 0xb5, 0x8b, 0x9a, 0x67, 0xb5, 0xb6, 0x62, 0x6d, 0xa1,
+	0xdb, 0x70, 0x78, 0xaf, 0x6a, 0xf5, 0xde, 0x45, 0x82, 0x53, 0xf3, 0x5b, 0x00, 0xfc, 0x80, 0xf0,
+	0xb1, 0xb7, 0x68, 0xfa, 0xe7, 0x9a, 0x2e, 0x2b, 0x3f, 0x92, 0x57, 0xb5, 0xe1, 0x8f, 0x02, 0x93,
+	0xbb, 0xed, 0x9e, 0xf8, 0xbb, 0x3d, 0xf1, 0xbf, 0xf6, 0xc4, 0x7f, 0x4b, 0x89, 0xb7, 0x4b, 0x89,
+	0xf7, 0x91, 0x12, 0xef, 0x89, 0x4a, 0x65, 0x97, 0x31, 0xa7, 0x0b, 0x1d, 0x32, 0x23, 0xa4, 0xdc,
+	0xbc, 0x24, 0x0c, 0x74, 0x18, 0x8a, 0x95, 0x12, 0x11, 0x4b, 0xc6, 0xec, 0x35, 0xbf, 0x07, 0xb3,
+	0x1b, 0x23, 0x80, 0x37, 0xdd, 0x59, 0xc6, 0xdf, 0x01, 0x00, 0x00, 0xff, 0xff, 0xf1, 0xb9, 0x59,
+	0xf6, 0x02, 0x02, 0x00, 0x00,
 }
 
 func (m *GenesisState) Marshal() (dAtA []byte, err error) {
@@ -111,6 +150,62 @@ func (m *GenesisState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.SubscriberIntents) > 0 {
+		for iNdEx := len(m.SubscriberIntents) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.SubscriberIntents[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintGenesis(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x2a
+		}
+	}
+	if len(m.PublisherIntents) > 0 {
+		for iNdEx := len(m.PublisherIntents) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.PublisherIntents[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintGenesis(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x22
+		}
+	}
+	if len(m.Subscribers) > 0 {
+		for iNdEx := len(m.Subscribers) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Subscribers[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintGenesis(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x1a
+		}
+	}
+	if len(m.Publishers) > 0 {
+		for iNdEx := len(m.Publishers) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Publishers[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintGenesis(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x12
+		}
+	}
 	{
 		size, err := m.Params.MarshalToSizedBuffer(dAtA[:i])
 		if err != nil {
@@ -143,6 +238,30 @@ func (m *GenesisState) Size() (n int) {
 	_ = l
 	l = m.Params.Size()
 	n += 1 + l + sovGenesis(uint64(l))
+	if len(m.Publishers) > 0 {
+		for _, e := range m.Publishers {
+			l = e.Size()
+			n += 1 + l + sovGenesis(uint64(l))
+		}
+	}
+	if len(m.Subscribers) > 0 {
+		for _, e := range m.Subscribers {
+			l = e.Size()
+			n += 1 + l + sovGenesis(uint64(l))
+		}
+	}
+	if len(m.PublisherIntents) > 0 {
+		for _, e := range m.PublisherIntents {
+			l = e.Size()
+			n += 1 + l + sovGenesis(uint64(l))
+		}
+	}
+	if len(m.SubscriberIntents) > 0 {
+		for _, e := range m.SubscriberIntents {
+			l = e.Size()
+			n += 1 + l + sovGenesis(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -211,6 +330,142 @@ func (m *GenesisState) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			if err := m.Params.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Publishers", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Publishers = append(m.Publishers, &Publisher{})
+			if err := m.Publishers[len(m.Publishers)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Subscribers", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Subscribers = append(m.Subscribers, &Subscriber{})
+			if err := m.Subscribers[len(m.Subscribers)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 4:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PublisherIntents", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PublisherIntents = append(m.PublisherIntents, &PublisherIntent{})
+			if err := m.PublisherIntents[len(m.PublisherIntents)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 5:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field SubscriberIntents", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.SubscriberIntents = append(m.SubscriberIntents, &SubscriberIntent{})
+			if err := m.SubscriberIntents[len(m.SubscriberIntents)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
