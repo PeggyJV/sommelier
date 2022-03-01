717f668ce764af91e8507b693ae0c9e67c54305f
Eric Bolten <eric@bolten.dev>
2022-02-28 21:25:58 -0800

    Keeper keys for basic pubsub types

--- x/pubsub/types/keys.go
+++ x/pubsub/types/keys.go
@@ -1,5 +1,11 @@
 package types
 
+import (
+	"bytes"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "pubsub"
@@ -15,3 +21,39 @@ const (
 )
 
 // TODO(bolten): fill out keys
+
+const (
+	_ = byte(iota)
+
+	// PublisherPrefix - <prefix><publisher_domain> -> Publisher
+	PublisherPrefix
+
+	// SubscriberPrefix - <prefix><subscriber_address> -> Subscriber
+	SubscriberPrefix
+
+	// PublisherIntentPrefix - <prefix><publisher_domain><subscription_id> -> PublisherIntent
+	PublisherIntentPrefix
+
+	// SubscriberIntentPrefix - <prefix><subscriber_address><subscription_id> -> SubscriberIntent
+	SubscriberIntentPrefix
+)
+
+// GetPublisherKey returns the key for a Publisher
+func GetPublisherKey(publisherDomain string) []byte {
+	return append([]byte{PublisherPrefix}, []byte(publisherDomain)...)
+}
+
+// GetSubscriberKey returns the key for a Subscriber
+func GetSubscriberKey(subscriberAddress sdk.Address) []byte {
+	return append([]byte{SubscriberPrefix}, subscriberAddress.Bytes()...)
+}
+
+// GetPublisherIntentKey returns the key for a PublisherIntent
+func GetPublisherIntentkey(publisherDomain string, subscriptionId string) []byte {
+	return bytes.Join([][]byte{{PublisherIntentPrefix}, []byte(publisherDomain), []byte(subscriptionId)}, []byte{})
+}
+
+// GetSubscriberIntentKey returns the key for a SubscriberIntent
+func GetSubscriberIntentKey(subscriberAddress sdk.Address, subscriptionId string) []byte {
+	return bytes.Join([][]byte{{SubscriberIntentPrefix}, subscriberAddress.Bytes(), []byte(subscriptionId)}, []byte{})
+}
