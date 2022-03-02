6d1da99c20dd27d07f0102049046bb74ed642203
Eric Bolten <eric@bolten.dev>
2022-03-02 13:53:09 -0800

    Remove TODO for Begin/EndBlocker, nothing yet

--- x/pubsub/keeper/abci.go
+++ x/pubsub/keeper/abci.go
@@ -2,7 +2,5 @@ package keeper
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
-// TODO(bolten): fill out BeginBlocker and EndBlocker
-
 func (k Keeper) BeginBlocker(ctx sdk.Context) {}
 func (k Keeper) EndBlocker(ctx sdk.Context)   {}
