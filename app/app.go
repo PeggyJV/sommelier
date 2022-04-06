1f3ebd8de3fa5a9935bef2cfc17df68e824a74a8
Eric Bolten <eric@bolten.dev>
2022-04-05 22:47:13 -0700

    Set up InitChainer properly

--- app/app.go
+++ app/app.go
@@ -566,6 +566,9 @@ func (app *SommelierApp) InitChainer(ctx sdk.Context, req abci.RequestInitChain)
 	if err := tmjson.Unmarshal(req.AppStateBytes, &genesisState); err != nil {
 		panic(err)
 	}
+
+	app.UpgradeKeeper.SetModuleVersionMap(ctx, app.mm.GetVersionMap())
+
 	return app.mm.InitGenesis(ctx, app.appCodec, genesisState)
 }
 
