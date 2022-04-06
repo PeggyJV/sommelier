59c4bc652198bef20f379363caf09f9cf436b4b8
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2022-04-06 15:11:43 -0400

    query cli commands

--- x/cork/client/cli/query.go
+++ x/cork/client/cli/query.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/peggyjv/sommelier/v3/x/cork/types"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 // GetQueryCmd returns the cli query commands for this module
@@ -18,7 +19,11 @@ func GetQueryCmd() *cobra.Command {
 
 	allocationQueryCmd.AddCommand([]*cobra.Command{
 		queryParams(),
-		queryVotePeriod(),
+		queryCommitPeriod(),
+		queryScheduledCorks(),
+		queryCellarIDs(),
+		queryScheduledBlockHeights(),
+		queryScheduledCorksByBlockHeight(),
 	}...)
 
 	return allocationQueryCmd
@@ -50,12 +55,12 @@ func queryParams() *cobra.Command {
 	}
 }
 
-func queryVotePeriod() *cobra.Command {
+func queryCommitPeriod() *cobra.Command {
 	return &cobra.Command{
-		Use:     "vote-period",
-		Aliases: []string{"vp"},
+		Use:     "commit-period",
+		Aliases: []string{"cp"},
 		Args:    cobra.NoArgs,
-		Short:   "query vote period data from the chain",
+		Short:   "query commit period data from the chain",
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			ctx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -66,6 +71,113 @@ func queryVotePeriod() *cobra.Command {
 			req := &types.QueryCommitPeriodRequest{}
 
 			res, err := queryClient.QueryCommitPeriod(cmd.Context(), req)
+			if err != nil {
+				return err
+			}
+
+			return ctx.PrintProto(res)
+		},
+	}
+}
+
+func queryCellarIDs() *cobra.Command {
+	return &cobra.Command{
+		Use:     "cellar-ids",
+		Aliases: []string{"cids"},
+		Args:    cobra.NoArgs,
+		Short:   "query managed cellar ids from the chain",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			ctx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(ctx)
+			req := &types.QueryCellarIDsRequest{}
+
+			res, err := queryClient.QueryCellarIDs(cmd.Context(), req)
+			if err != nil {
+				return err
+			}
+
+			return ctx.PrintProto(res)
+		},
+	}
+}
+
+func queryScheduledCorks() *cobra.Command {
+	return &cobra.Command{
+		Use:     "scheduled-corks",
+		Aliases: []string{"scs"},
+		Args:    cobra.NoArgs,
+		Short:   "query scheduled corks from the chain",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			ctx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(ctx)
+			req := &types.QueryScheduledCorksRequest{}
+
+			res, err := queryClient.QueryScheduledCorks(cmd.Context(), req)
+			if err != nil {
+				return err
+			}
+
+			return ctx.PrintProto(res)
+		},
+	}
+}
+
+func queryScheduledCorksByBlockHeight() *cobra.Command {
+	return &cobra.Command{
+		Use:     "scheduled-corks-by-block-height",
+		Aliases: []string{"scbbh"},
+		Args:    cobra.ExactArgs(1),
+		Short:   "query scheduled corks from the chain by block height",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			height, err := strconv.Atoi(args[0])
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(ctx)
+			req := &types.QueryScheduledCorksByBlockHeightRequest{
+				BlockHeight: uint64(height),
+			}
+
+			res, err := queryClient.QueryScheduledCorksByBlockHeight(cmd.Context(), req)
+			if err != nil {
+				return err
+			}
+
+			return ctx.PrintProto(res)
+		},
+	}
+}
+
+func queryScheduledBlockHeights() *cobra.Command {
+	return &cobra.Command{
+		Use:     "scheduled-block-heights",
+		Aliases: []string{"scbhs"},
+		Args:    cobra.NoArgs,
+		Short:   "query scheduled cork block heights from the chain",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			ctx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(ctx)
+			req := &types.QueryScheduledBlockHeightsRequest{}
+
+			res, err := queryClient.QueryScheduledBlockHeights(cmd.Context(), req)
 			if err != nil {
 				return err
 			}
