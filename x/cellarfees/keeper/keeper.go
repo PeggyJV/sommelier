644eedc136003523b3e0e9f69eed43803d265bf8
Collin Brittain <collinbrittain@gmail.com>
2022-08-11 16:15:14 -0500

    Switch to using sdk.Coin(s) methods for accounting in pool

--- x/cellarfees/keeper/keeper.go
+++ x/cellarfees/keeper/keeper.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/tendermint/tendermint/libs/log"
 
@@ -91,70 +90,31 @@ func (k Keeper) SetCellarFeePool(ctx sdk.Context, cellarFeePool types.CellarFeeP
 // Appends the coin to the pool coins if denom isn't already present, otherwise add the amount to the
 // existing balance
 func (k Keeper) AddCoinToPool(ctx sdk.Context, coin sdk.Coin) {
-	if coin.Denom == params.BaseCoinUnit || coin.Denom == params.HumanCoinUnit {
-		panic("Cannot add SOMM to cellar fee pool")
-	}
-	if coin.IsZero() {
+	if (coin == sdk.Coin{}) {
 		return
 	}
 
-	pool := k.GetCellarFeePool(ctx)
-	i, balance := k.FindBalanceInPool(ctx, coin.Denom, pool.Pool)
-	if i == -1 {
-		pool.Pool = append(pool.Pool, coin)
-	} else {
-		balance = balance.Add(coin)
-		pool.Pool[i] = balance
+	if coin.Denom == params.BaseCoinUnit || coin.Denom == params.HumanCoinUnit {
+		panic("Cannot add SOMM to cellar fee pool")
 	}
 
+	pool := k.GetCellarFeePool(ctx)
+	pool.Pool = pool.Pool.Add(coin)
 	k.SetCellarFeePool(ctx, pool)
 }
 
 func (k Keeper) AddCoinsToPool(ctx sdk.Context, coins sdk.Coins) {
-	pool := k.GetCellarFeePool(ctx)
 	if len(coins) == 0 {
 		return
 	}
 
-	for _, coin := range coins {
-		if coin.Denom == params.BaseCoinUnit || coin.Denom == params.HumanCoinUnit {
-			panic("Cannot add SOMM to cellar fee pool")
-		}
-
-		i, balance := k.FindBalanceInPool(ctx, coin.Denom, pool.Pool)
-		if i == -1 {
-			pool.Pool = append(pool.Pool, coin)
-		} else {
-			balance = balance.Add(coin)
-			pool.Pool[i] = balance
-		}
+	if !coins.AmountOfNoDenomValidation(params.BaseCoinUnit).IsZero() {
+		panic("Cannot add SOMM to cellar fee pool")
 	}
 
+	pool := k.GetCellarFeePool(ctx)
+	pool.Pool = pool.Pool.Add(coins.Sort()...)
 	k.SetCellarFeePool(ctx, pool)
-}
-
-func (k Keeper) IteratePoolBalances(ctx sdk.Context, handler func(coin sdk.Coin) (stop bool)) {
-	pool := k.GetCellarFeePool(ctx).Pool
-	for _, coin := range pool {
-		if handler(coin) {
-			return
-		}
-	}
-}
-
-// Retrieve an Coin and its index from the provided pool if it is present, otherwise return
-// and empty Coin and an index of -1. This approach helps avoid accidentally reading the same
-// pool from the store over and over in cases like AddCoinsToPool.
-func (k Keeper) FindBalanceInPool(ctx sdk.Context, denom string, pool sdk.Coins) (int, sdk.Coin) {
-	i := sort.Search(len(pool), func(i int) bool {
-		return pool[i].Denom == denom
-	})
-
-	if i == len(pool) {
-		return -1, sdk.Coin{}
-	}
-
-	return i, pool[i]
 }
 
 func (k Keeper) SendPoolToAuction(ctx sdk.Context) {
--- x/cellarfees/keeper/keeper_test.go
+++ x/cellarfees/keeper/keeper_test.go
@@ -18,7 +18,7 @@ func TestInitializingPool(t *testing.T) {
 	require.Equal(t, len(pool), 0)
 }
 
-func TestGettingSettingSearchingCellarFeePool(t *testing.T) {
+func TestGettingSettingCellarFeePool(t *testing.T) {
 	env := CreateTestEnv(t)
 	ctx := env.Context
 
@@ -36,15 +36,10 @@ func TestGettingSettingSearchingCellarFeePool(t *testing.T) {
 	}
 
 	pool := env.cellarFeesKeeper.GetCellarFeePool(ctx)
-	i, balance := env.cellarFeesKeeper.FindBalanceInPool(ctx, denom, pool.Pool)
-	require.Equal(t, -1, i)
-	require.Zero(t, balance)
-	require.Equal(t, types.CellarFeePool{}, pool)
+	require.True(t, pool.Pool.Empty())
 
 	env.cellarFeesKeeper.SetCellarFeePool(ctx, expectedPool)
 	pool = env.cellarFeesKeeper.GetCellarFeePool(ctx)
-	i, _ = env.cellarFeesKeeper.FindBalanceInPool(ctx, denom, pool.Pool)
-	require.Equal(t, 0, i)
 	require.Equal(t, expectedPool, pool)
 }
 
@@ -61,15 +56,58 @@ func TestAddingCoinsToPool(t *testing.T) {
 		Denom:  denom0,
 	}
 
+	// adding zeroed coin
+	poolBefore := env.cellarFeesKeeper.GetCellarFeePool(ctx)
+	env.cellarFeesKeeper.AddCoinToPool(ctx, sdk.Coin{})
+	poolAfter := env.cellarFeesKeeper.GetCellarFeePool(ctx)
+	require.Equal(t, poolBefore, poolAfter)
+
+	// appending new coin
 	env.cellarFeesKeeper.AddCoinToPool(ctx, single)
 	pool := env.cellarFeesKeeper.GetCellarFeePool(ctx)
-	require.Equal(t, single, pool.Pool[0])
+	require.Equal(t, sdk.Coins{single}, pool.Pool)
 
+	// adding to existing coin
 	env.cellarFeesKeeper.AddCoinToPool(ctx, single)
 	pool = env.cellarFeesKeeper.GetCellarFeePool(ctx)
 	expected := single.Add(single)
 	require.Equal(t, expected, pool.Pool[0])
 
+	// adding zeroed coins
+	poolBefore = env.cellarFeesKeeper.GetCellarFeePool(ctx)
+	env.cellarFeesKeeper.AddCoinsToPool(ctx, sdk.Coins{})
+	poolAfter = env.cellarFeesKeeper.GetCellarFeePool(ctx)
+	require.Equal(t, poolBefore, poolAfter)
+
+	denom1 := "gravity-0x1111111111111111111111111111111111111111"
+	multi := sdk.Coins{
+		{
+
+			Amount: sdk.NewIntFromUint64(100),
+			Denom:  denom1,
+		},
+		{
+			Amount: sdk.NewIntFromUint64(100),
+			Denom:  denom0,
+		},
+	}
+
+	// appends one and adds to existing
+	env.cellarFeesKeeper.AddCoinsToPool(ctx, multi)
+	pool = env.cellarFeesKeeper.GetCellarFeePool(ctx)
+	expectedPool := multi.Sort()
+	expectedPool[0] = expectedPool[0].Add(single).Add(single)
+	require.Equal(t, expectedPool, pool.Pool)
+}
+
+func TestTransferringFeesToAuction(t *testing.T) {
+	env := CreateTestEnv(t)
+	ctx := env.Context
+
+	// initialize pool
+	env.cellarFeesKeeper.SetCellarFeePool(ctx, types.NewEmptyPool())
+
+	denom0 := "gravity-0x0000000000000000000000000000000000000000"
 	denom1 := "gravity-0x1111111111111111111111111111111111111111"
 	multi := sdk.Coins{
 		{
@@ -83,10 +121,18 @@ func TestAddingCoinsToPool(t *testing.T) {
 		},
 	}
 
+	// simulate coins being added to module account
+	env.BankKeeper.MintCoins(ctx, types.ModuleName, multi)
+
+	// nothing should happen because pool is empty
+	env.cellarFeesKeeper.SendPoolToAuction(ctx)
+
+	// update pool with moduel account balance
 	env.cellarFeesKeeper.AddCoinsToPool(ctx, multi)
-	pool = env.cellarFeesKeeper.GetCellarFeePool(ctx)
-	expected = expected.Add(multi[0])
-	require.Equal(t, 2, len(pool.Pool))
-	require.Equal(t, expected, pool.Pool[0])
-	require.Equal(t, multi[1], pool.Pool[1])
+	env.cellarFeesKeeper.SendPoolToAuction(ctx)
+
+	pool := env.cellarFeesKeeper.GetCellarFeePool(ctx)
+	balances := env.BankKeeper.GetAllBalances(ctx, env.AccountKeeper.GetModuleAddress("auction"))
+	require.Equal(t, multi, balances)
+	require.Equal(t, 0, len(pool.Pool))
 }
