2f1a66ad49fb91752c0eca2eaa82d380fffe89a7
Collin Brittain <collinbrittain@gmail.com>
2022-08-15 06:13:26 -0500

    Ignore SOMM in cellarfees AfterSendToCosmosEvent handler.

--- x/cellarfees/keeper/hooks.go
+++ x/cellarfees/keeper/hooks.go
@@ -4,6 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
 	gravitytypes "github.com/peggyjv/gravity-bridge/module/v2/x/gravity/types"
+	"github.com/peggyjv/sommelier/v4/app/params"
 	"github.com/peggyjv/sommelier/v4/x/cellarfees/types"
 )
 
@@ -29,8 +30,8 @@ func (h Hooks) AfterSignerSetExecutedEvent(ctx sdk.Context, event gravitytypes.S
 func (h Hooks) AfterBatchExecutedEvent(ctx sdk.Context, event gravitytypes.BatchExecutedEvent) {}
 
 // In order to update the CellarFeePool, we check gravity module SendToCosmos transactions to see if the recipient is
-// the cellarfees module account, if the sender is a Cellar contract approved by governance, [and if the fee token is
-// approved by governance]. If all of these cases are true, we add the fee to the pool.
+// the cellarfees module account, and if the sender is a Cellar contract approved by governance. If these criteria are
+// met, we account for the coins as fees in the pool.
 func (h Hooks) AfterSendToCosmosEvent(ctx sdk.Context, event gravitytypes.SendToCosmosEvent) {
 	// Check if recipient is the cellarfees module account
 	moduleAccountAddress := h.k.accountKeeper.GetModuleAddress(types.ModuleName)
@@ -41,6 +42,12 @@ func (h Hooks) AfterSendToCosmosEvent(ctx sdk.Context, event gravitytypes.SendTo
 	// Check if the sender is an approved Cellar contract
 	if h.k.corkKeeper.HasCellarID(ctx, common.HexToAddress(event.EthereumSender)) {
 		_, denom := h.k.gravityKeeper.ERC20ToDenomLookup(ctx, common.HexToAddress(event.TokenContract))
+
+		// don't include SOMM in the pool
+		if denom == params.BaseCoinUnit {
+			return
+		}
+
 		balance := h.k.bankKeeper.GetBalance(ctx, moduleAccountAddress, denom)
 
 		// sanity check
