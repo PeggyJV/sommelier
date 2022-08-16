8bebb7eadc9250295eba0357ac30b1c107e86b48
Eric Bolten <eric@bolten.dev>
2022-08-16 10:20:40 -0700

    More types work

---
+++ x/auction/types/events.go
@@ -0,0 +1,19 @@
+package types
+
+// auction module event types
+const (
+	// TODO: fill in with event types and their attributes, examples below
+	/*
+		EventTypeCork         = "cork"
+
+		AttributeKeySigner            = "signer"
+		AttributeKeyValidator         = "validator"
+		AttributeKeyPrevoteHash       = "hash"
+		AttributeKeyCork              = "cork"
+		AttributeKeyCommitPeriodStart = "commit_period_start"
+		AttributeKeyCommitPeriodEnd   = "commit_period_end"
+		AttributeKeyBlockHeight       = "block_height"
+	*/
+
+	AttributeValueCategory = ModuleName
+)
