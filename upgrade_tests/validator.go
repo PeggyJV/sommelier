251590c385660aa6cb4c87eda03e1b9c8d4c04dd
hannydevelop <ukpaiugochiibem@gmail.com>
2022-08-22 20:05:59 +0100

    created upgrade test folder

---
+++ upgrade_tests/validator.go
@@ -0,0 +1,3 @@
+package upgrade_test
+
+// instead of mounts in the integration test example, use `ghcr.io/strangelove-ventures/heighliner/sommelier` as repository and `v4.0.1` or other version as tag.
