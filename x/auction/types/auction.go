9270834893024536309e225fb9db6d9f18887da7
Phil <pbal@fordham.edu>
2022-08-23 16:19:49 -0400

    Update auction type interfaces & fix typo

--- x/auction/types/auction.go
+++ x/auction/types/auction.go
@@ -8,12 +8,74 @@ import (
 )
 
 func (a *Auction) Equals(other Auction) bool {
-	// TODO: fill in
-	return false
+	if a.Id != other.Id {
+		return false
+	}
+
+	if !a.StartingAmount.IsEqual(other.StartingAmount) {
+		return false
+	}
+	
+	if a.StartBlock != other.StartBlock {
+		return false
+	}
+
+	if a.EndBlock != other.EndBlock {
+		return false
+	}
+
+	if a.InitialDecreaseRate != other.InitialDecreaseRate {
+		return false
+	}
+
+	if a.CurrentDecreaseRate != other.CurrentDecreaseRate {
+		return false
+	}
+
+	if a.BlockDecreaseInterval != other.BlockDecreaseInterval {
+		return false
+	}
+
+	if !a.CurrentPrice.IsEqual(other.CurrentPrice) {
+		return false
+	}
+
+	if !a.AmountRemaining.IsEqual(other.AmountRemaining) {
+		return false
+	}
+
+	return true
 }
 
 func (a *Auction) ValidateBasic() error {
-	// TODO: fill in
+	if a.Id == 0 {
+		return fmt.Errorf("auction IDs must be non-zero")
+	}
+
+	if !a.StartingAmount.IsPositive() {
+		return fmt.Errorf("minimum amount must be a positive amount of coins")
+	}
+
+	if a.StartBlock <= 0 {
+		return fmt.Errorf("start block must be a positive")
+	}
+
+	if a.EndBlock <= 0 {
+		return fmt.Errorf("end block must be a positive")
+	}
+
+	if a.StartBlock > a.EndBlock {
+		return fmt.Errorf("start block must be smaller or equal to end block")
+	}
+
+	if a.InitialDecreaseRate <= 0 || a.InitialDecreaseRate >= 1 {
+		return fmt.Errorf("initial decrease rate must be a float less than one and greater than zero")
+	}
+
+	if a.CurrentDecreaseRate < 0 || a.CurrentDecreaseRate > 1 {
+		return fmt.Errorf("current decrease rate must be a float less than or equal to one and greater than or equal to zero")
+	}
+
 	return nil
 }
 
@@ -68,21 +130,45 @@ func (b *Bid) ValidateBasic() error {
 }
 
 func (f *FulfilledBid) Equals(other FulfilledBid) bool {
-	// TODO: fill in
-	return false
+	if !f.Bid.Equals(*other.Bid) {
+		return false
+	}
+
+	if !f.FulfilledAmount.IsEqual(other.FulfilledAmount) {
+		return false
+	}
+
+	if !f.FulfillmentPrice.IsEqual(other.FulfillmentPrice) {
+		return false
+	}
+
+	return true
 }
 
 func (f *FulfilledBid) ValidateBasic() error {
-	// TODO: fill in
 	return nil
 }
 
 func (t *TokenPrice) Equals(other TokenPrice) bool {
-	// TODO: fill in
-	return false
+	if t.Denom != other.Denom {
+		return false
+	}
+
+	if t.UsdPrice != other.UsdPrice {
+		return false
+	}
+
+	return true
 }
 
 func (t *TokenPrice) ValidateBasic() error {
-	// TODO: fill in
+	if t.Denom == "" {
+		return fmt.Errorf("denom must be a non empty string")
+	}
+
+	if t.UsdPrice.IsNegative() {
+		return fmt.Errorf("price must be greater than or equal to 0")
+	}
+
 	return nil
 }
