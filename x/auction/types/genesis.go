9efb1722e7281eee67cbc9d4dad228143fe37058
Phil <pbal@fordham.edu>
2022-08-23 16:22:47 -0400

    Update genesis state validation

--- x/auction/types/genesis.go
+++ x/auction/types/genesis.go
@@ -11,10 +11,26 @@ func DefaultGenesisState() GenesisState {
 
 // Validate performs a basic stateless validation of the genesis fields.
 func (gs GenesisState) Validate() error {
-	// TODO: validate auctions, bids, tokenprices
-
 	if err := gs.Params.ValidateBasic(); err != nil {
 		return err
+	}
+
+	for _, auction := range gs.Auctions {
+		if err := auction.ValidateBasic(); err != nil {
+			return err
+		}
+	}
+	
+	for _, bid := range gs.Bids {
+		if err := bid.ValidateBasic(); err != nil {
+			return err
+		}
+	}
+
+	for _, tokenPrice := range gs.TokenPrices {
+		if err := tokenPrice.ValidateBasic(); err != nil {
+			return err
+		}
 	}
 
 	return nil
