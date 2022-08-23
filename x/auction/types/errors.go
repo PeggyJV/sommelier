cf67dbcdbf021a5b85ceb583d9f4d18d3d4606dc
Phil <pbal@fordham.edu>
2022-08-23 16:19:49 -0400

    Update auction type interfaces & fix typo

--- x/auction/types/errors.go
+++ x/auction/types/errors.go
@@ -6,7 +6,7 @@ import (
 )
 */
 
-// x/auctiojn module sentinel errors
+// x/auction module sentinel errors
 var (
 // TODO: fill in errors, below is an example, start with ID 2
 //ErrInvalidAddress         = sdkerrors.Register(ModuleName, 2, "invalid ethereum address")
