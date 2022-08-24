e65bd35e64e064c4c31a342c085c38e0b4b4d620
hannydevelop <ukpaiugochiibem@gmail.com>
2022-08-24 17:16:06 +0100

    validator file ready

--- upgrade_tests/chain.go
+++ upgrade_tests/chain.go
@@ -1,1 +1,29 @@
 package upgrade_test
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/peggyjv/sommelier/v4/app/params"
+)
+
+const (
+	keyringPassphrase = "testpassphrase"
+	keyringAppName    = "testnet"
+)
+
+var (
+	encodingConfig params.EncodingConfig
+	cdc            codec.Codec
+)
+
+type chain struct {
+	dataDir       string
+	id            string
+	validators    []*validator
+	orchestrators []*orchestrator
+}
+
+func (c *chain) configDir() string {
+	return fmt.Sprintf("%s/%s", c.dataDir, c.id)
+}
--- upgrade_tests/genesis.go
+++ upgrade_tests/genesis.go
@@ -1,1 +1,33 @@
 package upgrade_test
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	tmtypes "github.com/tendermint/tendermint/types"
+)
+
+func getGenDoc(path string) (*tmtypes.GenesisDoc, error) {
+	serverCtx := server.NewDefaultContext()
+	config := serverCtx.Config
+	config.SetRoot(path)
+
+	genFile := config.GenesisFile()
+	doc := &tmtypes.GenesisDoc{}
+
+	if _, err := os.Stat(genFile); err != nil {
+		if !os.IsNotExist(err) {
+			return nil, err
+		}
+	} else {
+		var err error
+
+		doc, err = tmtypes.GenesisDocFromFile(genFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read genesis doc from file: %w", err)
+		}
+	}
+
+	return doc, nil
+}
