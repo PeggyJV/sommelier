3275203820d38f5dd0985141e0c903014f69ec1e
hannydevelop <ukpaiugochiibem@gmail.com>
2022-08-24 14:42:08 +0100

    orchestrator file containing instanceName ready

--- upgrade_tests/orchestrator.go
+++ upgrade_tests/orchestrator.go
@@ -1,1 +1,18 @@
 package upgrade_test
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+)
+
+type orchestrator struct {
+	index    int
+	mnemonic string
+	keyInfo  keyring.Info
+	keyring  *keyring.Keyring
+}
+
+func (o *orchestrator) instanceName() string {
+	return fmt.Sprintf("orchestrator%d", o.index)
+}
