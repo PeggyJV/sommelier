c2b8ad585ccf619fd6f9f07218e0de4a020340cf
Phil <pbal@fordham.edu>
2022-08-24 13:25:21 -0400

    Fix typos and finish stubbing out keeper

--- x/auction/keeper/keeper.go
+++ x/auction/keeper/keeper.go
@@ -3,10 +3,10 @@ package keeper
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/peggyjv/sommelier/v4/x/auction/types"
 	"github.com/tendermint/tendermint/libs/log"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 // Keeper of the auction store
@@ -60,23 +60,43 @@ func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 // Auctions //
 //////////////
 
-// GetAuctions returns all stored auctions
-func (k Keeper) GetAuctions(ctx sdk.Context) []*types.Auction  {
+// GetAllAuctions returns all stored auctions
+func (k Keeper) GetAllAuctions(ctx sdk.Context) []*types.Auction {
 	// TODO: Fill in
 	return nil
 }
 
-// SetAuctions sets the auctions specified 
-func (k Keeper) SetAuctions(ctx sdk.Context, auctions[] *types.Auction) {
+// GetAuctionById returns a specific auction
+func (k Keeper) GetAuctionsById(ctx sdk.Context, id uint32) *types.Auction {
+	// TODO: Fill in
+	return nil
+}
+
+// GetAllActiveAuctions returns all active auctions
+func (k Keeper) GetAllActiveAuctions(ctx sdk.Context) []*types.Auction {
+	// TODO: Fill in
+	return nil
+}
+
+
+// GetAllInactiveAuctions returns all inactive auctions (that have not been pruned)
+func (k Keeper) GetAllInactiveAuctions(ctx sdk.Context, id uint32) []*types.Auction {
+	// TODO: Fill in
+	return nil
+}
+
+
+// SetAuctions sets the auctions specified
+func (k Keeper) SetAuctions(ctx sdk.Context, auctions []*types.Auction) {
 	// TODO: Fill in
 }
 
 // BeginAuction starts a new auction for a single denomination
-func (k Keeper) BeginAuction(ctx sdk.Context, 
-	startingAmount sdk.Coin, 
-	initialDecreaseRate float32, 
-	blockDecreaseInterval uint16, 
-	fundingModuleAccount authtypes.ModuleAccountI, 
+func (k Keeper) BeginAuction(ctx sdk.Context,
+	startingAmount sdk.Coin,
+	initialDecreaseRate float32,
+	blockDecreaseInterval uint16,
+	fundingModuleAccount authtypes.ModuleAccountI,
 	proceeedsModuleAccount authtypes.AccountI) error {
 	// TODO: Verify inputs as first step, return error if problematic
 
@@ -89,15 +109,21 @@ func (k Keeper) BeginAuction(ctx sdk.Context,
 //   Bids   //
 //////////////
 
-// GetBids returns all stored bids
-func (k Keeper) GetBids(ctx sdk.Context) []*types.Bid  {
+// GetBids returns all stored bids (that have not been pruned)
+func (k Keeper) GetBids(ctx sdk.Context) []*types.Bid {
 	// TODO: Fill in
 	return nil
 }
 
-// SetBids sets the bids specified 
-func (k Keeper) SetBids(ctx sdk.Context, bids[] *types.Bid) {
+// SetBids sets the bids specified
+func (k Keeper) SetBids(ctx sdk.Context, bids []*types.Bid) {
 	// TODO: Fill in
+}
+
+// GetBid returns a specified bid by its id (if it has not been pruned)
+func (k Keeper) GetBid(ctx sdk.Context, id uint64) *types.Bid {
+	// TODO: Fill in
+	return nil
 }
 
 /////////////////
@@ -105,15 +131,12 @@ func (k Keeper) SetBids(ctx sdk.Context, bids[] *types.Bid) {
 /////////////////
 
 // GetTokenPrices returns all stored token prices
-func (k Keeper) GetTokenPrices(ctx sdk.Context) []*types.TokenPrice  {
+func (k Keeper) GetTokenPrices(ctx sdk.Context) []*types.TokenPrice {
 	// TODO: Fill in
 	return nil
 }
 
-// SetTokenPrices sets the token prices specified 
-func (k Keeper) SetTokenPrices(ctx sdk.Context, tokenPrices[] *types.TokenPrice) {
+// SetTokenPrices sets the token prices specified
+func (k Keeper) SetTokenPrices(ctx sdk.Context, tokenPrices []*types.TokenPrice) {
 	// TODO: Fill in
 }
-
-
-// TODO: implement the keeper functions!
