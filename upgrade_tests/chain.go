4c3b6121f7d67cd8c555a3e0afde7cedaa044272
hannydevelop <ukpaiugochiibem@gmail.com>
2022-08-25 17:40:23 +0100

    chain.go file ready

--- upgrade_tests/chain.go
+++ upgrade_tests/chain.go
@@ -2,9 +2,29 @@ package upgrade_test
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdkTypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkTx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	gravitytypes "github.com/peggyjv/gravity-bridge/module/v2/x/gravity/types"
 	"github.com/peggyjv/sommelier/v4/app/params"
+	corktypes "github.com/peggyjv/sommelier/v4/x/cork/types"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
 )
 
 const (
@@ -26,4 +46,179 @@ type chain struct {
 
 func (c *chain) configDir() string {
 	return fmt.Sprintf("%s/%s", c.dataDir, c.id)
+}
+
+func newChain() (*chain, error) {
+	var dir string
+	var err error
+	if _, found := os.LookupEnv("CI"); found {
+		dir, err = os.Getwd()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	tmpDir, err := ioutil.TempDir(dir, "somm-e2e-testnet")
+	if err != nil {
+		return nil, err
+	}
+
+	return &chain{
+		id:      "chain-" + tmrand.NewRand().Str(6),
+		dataDir: tmpDir,
+	}, nil
+}
+
+func (c *chain) createAndInitValidators(count int) error { // nolint:unused
+	for i := 0; i < count; i++ {
+		node := c.createValidator(i)
+
+		// generate genesis files
+		if err := node.init(); err != nil {
+			return err
+		}
+
+		c.validators = append(c.validators, node)
+
+		// create keys
+		if err := node.createKey("val"); err != nil {
+			return err
+		}
+		if err := node.createNodeKey(); err != nil {
+			return err
+		}
+		if err := node.createConsensusKey(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *chain) createAndInitValidatorsWithMnemonics(mnemonics []string) error {
+	for i := 0; i < len(mnemonics); i++ {
+		// create node
+		node := c.createValidator(i)
+
+		// generate genesis files
+		if err := node.init(); err != nil {
+			return err
+		}
+
+		c.validators = append(c.validators, node)
+
+		// create keys
+		if err := node.createKeyFromMnemonic("val", mnemonics[i], ""); err != nil {
+			return err
+		}
+		if err := node.createNodeKey(); err != nil {
+			return err
+		}
+		if err := node.createConsensusKey(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *chain) createAndInitOrchestrators(count int) error { // nolint:unused
+	mnemonics := make([]string, count)
+	for i := 0; i < count; i++ {
+		mnemonic, err := createMnemonic()
+		if err != nil {
+			return err
+		}
+		mnemonics = append(mnemonics, mnemonic)
+	}
+
+	return c.createAndInitOrchestratorsWithMnemonics(mnemonics)
+}
+
+func (c *chain) createAndInitOrchestratorsWithMnemonics(mnemonics []string) error {
+	hdPath := hd.CreateHDPath(sdk.CoinType, 1, 0)
+
+	for i := 0; i < len(mnemonics); i++ {
+		// create orchestrator
+		orchestrator := c.createOrchestrator(i)
+
+		// create keys
+		info, kb, err := createMemoryKeyFromMnemonic("orch", mnemonics[i], "", hdPath)
+		if err != nil {
+			return err
+		}
+
+		orchestrator.keyInfo = *info
+		orchestrator.mnemonic = mnemonics[i]
+		orchestrator.keyring = kb
+
+		c.orchestrators = append(c.orchestrators, orchestrator)
+	}
+
+	return nil
+}
+
+func (c *chain) createValidator(index int) *validator {
+	return &validator{
+		chain:   c,
+		index:   index,
+		moniker: "sommelier",
+	}
+}
+
+func (c *chain) createOrchestrator(index int) *orchestrator {
+	return &orchestrator{
+		index: index,
+	}
+}
+
+func (c *chain) clientContext(nodeURI string, kb *keyring.Keyring, fromName string, fromAddr sdk.AccAddress) (*client.Context, error) { // nolint:unparam
+	amino := codec.NewLegacyAmino()
+	interfaceRegistry := sdkTypes.NewInterfaceRegistry()
+	interfaceRegistry.RegisterImplementations((*sdk.Msg)(nil),
+		&stakingtypes.MsgCreateValidator{},
+		&gravitytypes.MsgDelegateKeys{},
+	)
+	interfaceRegistry.RegisterImplementations((*govtypes.Content)(nil),
+		&corktypes.AddManagedCellarIDsProposal{},
+		&corktypes.RemoveManagedCellarIDsProposal{},
+	)
+	interfaceRegistry.RegisterImplementations((*cryptotypes.PubKey)(nil), &secp256k1.PubKey{}, &ed25519.PubKey{})
+
+	protoCodec := codec.NewProtoCodec(interfaceRegistry)
+	txCfg := sdkTx.NewTxConfig(protoCodec, sdkTx.DefaultSignModes)
+
+	encodingConfig := params.EncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Marshaler:         protoCodec,
+		TxConfig:          txCfg,
+		Amino:             amino,
+	}
+	simapp.ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	simapp.ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+
+	rpcClient, err := rpchttp.New(nodeURI, "/websocket")
+	if err != nil {
+		return nil, err
+	}
+
+	clientContext := client.Context{}.
+		WithChainID(c.id).
+		WithCodec(protoCodec).
+		WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
+		WithTxConfig(encodingConfig.TxConfig).
+		WithLegacyAmino(encodingConfig.Amino).
+		WithInput(os.Stdin).
+		WithNodeURI(nodeURI).
+		WithClient(rpcClient).
+		WithBroadcastMode(flags.BroadcastBlock).
+		WithKeyring(*kb).
+		WithAccountRetriever(authtypes.AccountRetriever{}).
+		WithOutputFormat("json").
+		WithFrom(fromName).
+		WithFromName(fromName).
+		WithFromAddress(fromAddr).
+		WithSkipConfirmation(true)
+
+	return &clientContext, nil
 }
