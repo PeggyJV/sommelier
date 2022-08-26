2ea7d733e158fe9cd7f0b3242928548851fae69b
Phil <pbal@fordham.edu>
2022-08-25 22:23:37 -0400

    Fix typos

--- x/auction/keeper/abci.go
+++ x/auction/keeper/abci.go
@@ -4,11 +4,13 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// TODO: fill out BeginBlocker and EndBlocker
-
 // BeginBlocker is called at the beginning of every block
 func (k Keeper) BeginBlocker(ctx sdk.Context) {
-	// TODO Step function for auction price updates
+	for _, auction := range k.GetActiveAuctions(ctx) {
+		// TODO Step function for auction price updates
+
+	}
+
 
 	// TODO: do we need to do anything with proposal voting here?
 }
@@ -16,4 +18,12 @@ func (k Keeper) BeginBlocker(ctx sdk.Context) {
 // EndBlocker is called at the end of every block
 func (k Keeper) EndBlocker(ctx sdk.Context) {
 	// TODO End Auction that have no funds left
+	for _, auction := range k.GetActiveAuctions(ctx) {
+		// TODO: Move auction to ended auctions list with updated fields
+
+		// TODO: Send proceeds to their appropriate destination module
+
+	}
+
+	// TODO: anything else? Trim down old auctions and bids maybe?
 }
