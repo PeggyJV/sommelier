a9e5ffce7e72ed79c5b9882625c04da120e3d68a
Phil <pbal@fordham.edu>
2022-09-02 18:48:00 -0400

    Finish updating keeper

--- x/auction/keeper/abci.go
+++ x/auction/keeper/abci.go
@@ -12,37 +12,13 @@ func (k Keeper) BeginBlocker(ctx sdk.Context) {}
 
 // EndBlocker is called at the end of every block
 func (k Keeper) EndBlocker(ctx sdk.Context) {
-	// End Auctions that have no funds left
+	// Auction price updates
 	for _, auction := range k.GetActiveAuctions(ctx) {
-		if auction.AmountRemaining.Amount.IsZero() {
-			err := k.FinishAuction(ctx, auction)
-
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
-
-	for _, auction := range k.GetActiveAuctions(ctx) {
-		if ((ctx.BlockHeight() - int64(auction.StartBlock)) % int64(auction.BlockDecreaseInterval)) == 0 {
+		if ((ctx.BlockHeight() - int64(auction.StartBlock)) % int64(auction.PriceDecreaseBlockInterval)) == 0 {
 			// TODO post MVP (pbal) Make a more intricate & responsive step function for auction price updates
 
-			// Note, if token prices change in state due to governance, they will be reflected below as well
-			// Also note we do not check price freshness below, freshness is only checked at auction start time
-			initialSaleTokenUSDPrice, found := k.GetTokenPrice(ctx, auction.StartingAmount.Denom)
-			if !found {
-				panic("no price data found for starting amount denom")
-			}
-
-			usommUSDPrice, found := k.GetTokenPrice(ctx, types.UsommDenom)
-			if !found {
-				panic("no price data found for usomm")
-			}
-
-			initialSaleTokenUnitPriceInUsomm := initialSaleTokenUSDPrice.UsdPrice.Quo(usommUSDPrice.UsdPrice)
-
 			// Simple constant decrease rate meant for MVP
-			priceDecreaseAmountInUsomm := initialSaleTokenUnitPriceInUsomm.Mul(sdk.MustNewDecFromStr(fmt.Sprintf("%f",auction.CurrentDecreaseRate)))
+			priceDecreaseAmountInUsomm := auction.InitialUnitPriceInUsomm.Mul(auction.CurrentPriceDecreaseRate)
 			newUnitPriceInUsomm := auction.CurrentUnitPriceInUsomm.Sub(priceDecreaseAmountInUsomm)
 
 			// If the new price would be non positive, finish the auction
@@ -54,35 +30,25 @@ func (k Keeper) EndBlocker(ctx sdk.Context) {
 				}
 			} else { // Otherwise update the auction with the newest price
 				// Note we are not truncating the unit price (if usomm is stronger than the fee token, users will have to bid for a minimum number of fee tokens to make a purchase)
+				lastPrice := auction.CurrentUnitPriceInUsomm
 				auction.CurrentUnitPriceInUsomm = newUnitPriceInUsomm
 
 				// Update stored auction
 				k.setActiveAuction(ctx, *auction)
 
 				// Emit event that auction has updated
-				ctx.EventManager().EmitEvents(
-					sdk.Events{
-						sdk.NewEvent(
-							sdk.EventTypeMessage,
-							sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-						),
-						sdk.NewEvent(
-							types.EventTypeAuctionUpdated,
-							sdk.NewAttribute(types.AttributeKeyAuctionID, fmt.Sprint(auction.Id)),
-							sdk.NewAttribute(types.AttributeKeyStartBlock, fmt.Sprint(auction.StartBlock)),
-							sdk.NewAttribute(types.AttributeKeyEndBlock, fmt.Sprint(auction.EndBlock)),
-							sdk.NewAttribute(types.AttributeKeyInitialDecreaseRate, fmt.Sprintf("%f", auction.InitialDecreaseRate)),
-							sdk.NewAttribute(types.AttributeKeyCurrentDecreaseRate, fmt.Sprintf("%f", auction.CurrentDecreaseRate)),
-							sdk.NewAttribute(types.AttributeKeyBlockDecreaseInterval, fmt.Sprint(auction.BlockDecreaseInterval)),
-							sdk.NewAttribute(types.AttributeKeyStartingDenom, auction.StartingAmount.Denom),
-							sdk.NewAttribute(types.AttributeKeyStartingAmount, auction.StartingAmount.Amount.String()),
-							sdk.NewAttribute(types.AttributeKeyAmountRemaining, auction.AmountRemaining.String()),
-						),
-					},
+				ctx.EventManager().EmitEvent(
+					sdk.NewEvent(
+						types.EventTypeAuctionUpdated,
+						sdk.NewAttribute(types.AttributeKeyAuctionID, fmt.Sprint(auction.Id)),
+						sdk.NewAttribute(types.AttributeKeyLastPrice, lastPrice.String()),
+						sdk.NewAttribute(types.AttributeKeyNewPrice, auction.CurrentUnitPriceInUsomm.String()),
+						sdk.NewAttribute(types.AttributeKeyCurrentDecreaseRate, auction.CurrentPriceDecreaseRate.String()),
+						sdk.NewAttribute(types.AttributeKeyBlockDecreaseInterval, fmt.Sprint(auction.PriceDecreaseBlockInterval)),
+					),
 				)
 			}
 		}
 	}
-
 	// TODO post MVP (pbal): prune bids and auctions
 }
--- x/auction/types/auction.go
+++ x/auction/types/auction.go
@@ -15,48 +15,52 @@ func (a *Auction) ValidateBasic() error {
 		return sdkerrors.Wrapf(ErrAuctionIDMustBeNonZero, "id: %d", a.Id)
 	}
 
-	if !a.StartingAmount.IsPositive() {
-		return sdkerrors.Wrapf(ErrAuctionStartingAmountMustBePositve, "Starting amount: %s", a.StartingAmount.String())
+	if !a.StartingTokensForSale.IsPositive() {
+		return sdkerrors.Wrapf(ErrAuctionStartingAmountMustBePositve, "Starting tokens for sale: %s", a.StartingTokensForSale.String())
 	}
 
-	if a.StartingAmount.Denom == "" {
-		return sdkerrors.Wrapf(ErrAuctionDenomInvalid, "Starting denom: %s", a.StartingAmount.String())
+	if a.StartingTokensForSale.Denom == "" {
+		return sdkerrors.Wrapf(ErrAuctionDenomInvalid, "Starting denom tokens for sale: %s", a.StartingTokensForSale.String())
 	}
 
-	if a.StartingAmount.Denom == UsommDenom {
-		return sdkerrors.Wrapf(ErrCannotAuctionUsomm, "Starting denom is: %s", UsommDenom)
+	if a.StartingTokensForSale.Denom == UsommDenom {
+		return sdkerrors.Wrapf(ErrCannotAuctionUsomm, "Starting denom tokens for sale: %s", UsommDenom)
 	}
 
 	if a.StartBlock == 0 {
-		return sdkerrors.Wrapf(ErrInvalidStartBlock, "block: %d", a.StartBlock)
+		return sdkerrors.Wrapf(ErrInvalidStartBlock, "start block: %d", a.StartBlock)
 	}
 
-	if a.InitialDecreaseRate <= 0 || a.InitialDecreaseRate >= 1 {
-		return sdkerrors.Wrapf(ErrInvalidInitialDecreaseRate, "Inital decrease rate %f", a.InitialDecreaseRate)
+	if a.InitialPriceDecreaseRate.LTE(sdk.NewDec(0)) || a.InitialPriceDecreaseRate.GTE(sdk.NewDec(1)) {
+		return sdkerrors.Wrapf(ErrInvalidInitialDecreaseRate, "Inital price decrease rate %s", a.InitialPriceDecreaseRate.String())
 	}
 
-	if a.CurrentDecreaseRate <= 0 || a.CurrentDecreaseRate >= 1 {
-		return sdkerrors.Wrapf(ErrInvalidCurrentDecreaseRate, "Current decrease rate %f", a.CurrentDecreaseRate)
+	if a.CurrentPriceDecreaseRate.LTE(sdk.NewDec(0)) || a.CurrentPriceDecreaseRate.GTE(sdk.NewDec(1)) {
+		return sdkerrors.Wrapf(ErrInvalidCurrentDecreaseRate, "Current price decrease rate %s", a.CurrentPriceDecreaseRate.String())
 	}
 
-	if a.BlockDecreaseInterval == 0 {
-		return sdkerrors.Wrapf(ErrInvalidBlockDecreaeInterval, "Block Decrease interval: %d", a.BlockDecreaseInterval)
+	if a.PriceDecreaseBlockInterval == 0 {
+		return sdkerrors.Wrapf(ErrInvalidBlockDecreaeInterval, "price decrease block interval: %d", a.PriceDecreaseBlockInterval)
+	}
+
+	if !a.InitialUnitPriceInUsomm.IsPositive() {
+		return sdkerrors.Wrapf(ErrPriceMustBePositive, "initial unit price in usomm: %s", a.InitialUnitPriceInUsomm.String())
 	}
 
 	if !a.CurrentUnitPriceInUsomm.IsPositive() {
-		return sdkerrors.Wrapf(ErrPriceMustBePositive, "current unit price: %s", a.CurrentUnitPriceInUsomm.String())
+		return sdkerrors.Wrapf(ErrPriceMustBePositive, "current unit price in usomm: %s", a.CurrentUnitPriceInUsomm.String())
 	}
 
-	if a.AmountRemaining.Denom == "" {
-		return sdkerrors.Wrapf(ErrDenomCannotBeEmpty, "amount remaining denom: %s", a.AmountRemaining.String())
+	if a.RemainingTokensForSale.Denom == "" {
+		return sdkerrors.Wrapf(ErrDenomCannotBeEmpty, "token for sale remaining: %s", a.RemainingTokensForSale.String())
 	}
 
 	if a.FundingModuleAccount == "" {
-		return sdkerrors.Wrapf(ErrUnauthorizedFundingModule, "Account: %s", a.FundingModuleAccount)
+		return sdkerrors.Wrapf(ErrUnauthorizedFundingModule, "funding module account: %s", a.FundingModuleAccount)
 	}
 
 	if a.ProceedsModuleAccount == "" {
-		return sdkerrors.Wrapf(ErrUnauthorizedFundingModule, "Account: %s", a.ProceedsModuleAccount)
+		return sdkerrors.Wrapf(ErrUnauthorizedFundingModule, "proceeds module account: %s", a.ProceedsModuleAccount)
 	}
 
 	return nil
@@ -79,32 +83,36 @@ func (b *Bid) ValidateBasic() error {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, err.Error())
 	}
 
-	if !b.MaxBid.IsPositive() {
-		return sdkerrors.Wrapf(ErrBidIDAmountMustBePositive, "bid amount: %s", b.MaxBid.String())
+	if !b.MaxBidInUsomm.IsPositive() {
+		return sdkerrors.Wrapf(ErrBidIDAmountMustBePositive, "bid amount in usomm: %s", b.MaxBidInUsomm.String())
 	}
 
-	if b.MaxBid.Denom != UsommDenom {
-		return sdkerrors.Wrapf(ErrBidMustBeInUsomm, "bid: %s", b.MaxBid.String())
+	if b.MaxBidInUsomm.Denom != UsommDenom {
+		return sdkerrors.Wrapf(ErrBidMustBeInUsomm, "bid: %s", b.MaxBidInUsomm.String())
 	}
 
-	if !strings.HasPrefix(b.MinimumAmount.Denom, gravitytypes.GravityDenomPrefix) {
-		return sdkerrors.Wrapf(ErrInvalidTokenBeingBidOn, "token: %s", b.MinimumAmount)
+	if !strings.HasPrefix(b.SaleTokenMinimumAmount.Denom, gravitytypes.GravityDenomPrefix) {
+		return sdkerrors.Wrapf(ErrInvalidTokenBeingBidOn, "sale token: %s", b.SaleTokenMinimumAmount)
 	}
 
-	if !b.MinimumAmount.IsPositive() {
-		return sdkerrors.Wrapf(ErrMinimumAmountMustBePositive, "amount: %s", b.MinimumAmount.String())
+	if !b.SaleTokenMinimumAmount.IsPositive() {
+		return sdkerrors.Wrapf(ErrMinimumAmountMustBePositive, "sale token amount: %s", b.SaleTokenMinimumAmount.String())
 	}
 
-	if b.TotalFulfilledSaleTokenAmount.Amount.IsNegative() {
-		return sdkerrors.Wrapf(ErrBidFulfilledSaleTokenAmountMustBeNonNegative, "amount: %s", b.TotalFulfilledSaleTokenAmount.String())
+	if b.TotalFulfilledSaleTokens.Amount.IsNegative() {
+		return sdkerrors.Wrapf(ErrBidFulfilledSaleTokenAmountMustBeNonNegative, "fulfilled sale token amount: %s", b.TotalFulfilledSaleTokens.String())
 	}
 
-	if !b.UnitPriceOfSaleTokenInUsomm.IsPositive() {
-		return sdkerrors.Wrapf(ErrBidUnitPriceInUsommMustBePositive, "unit price: %s", b.UnitPriceOfSaleTokenInUsomm.String())
+	if !b.SaleTokenUnitPriceInUsomm.IsPositive() {
+		return sdkerrors.Wrapf(ErrBidUnitPriceInUsommMustBePositive, "sale token unit price: %s", b.SaleTokenUnitPriceInUsomm.String())
 	}
 
-	if b.TotalAmountPaidInUsomm.IsNegative() {
-		return sdkerrors.Wrapf(ErrBidPaymentCannotBeNegative, "payment: %s", b.TotalAmountPaidInUsomm.String())
+	if b.TotalUsommPaid.IsNegative() {
+		return sdkerrors.Wrapf(ErrBidPaymentCannotBeNegative, "payment in usomm: %s", b.TotalUsommPaid.String())
+	}
+
+	if b.TotalUsommPaid.Denom != UsommDenom {
+		return sdkerrors.Wrapf(ErrBidMustBeInUsomm, "payment denom: %s", b.TotalUsommPaid.Denom)
 	}
 
 	return nil
