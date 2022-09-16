b013a73f925e75ddaa6e5fc8d9fd83b5461ca022
Phil <pbal@fordham.edu>
2022-09-15 21:52:12 -0600

    Add params validate test

--- x/auction/types/errors.go
+++ x/auction/types/errors.go
@@ -40,4 +40,6 @@ var (
 	ErrBidUnitPriceInUsommMustBePositive                        = sdkerrors.Register(ModuleName, 33, "unit price of sale tokens in usomm must be positive")
 	ErrInvalidTokenPriceDenom                                   = sdkerrors.Register(ModuleName, 34, "token price denoms must be either usomm or addresses prefixed with 'gravity'")
 	ErrTokenPriceProposalAttemptsToUpdateTokenPriceMoreThanOnce = sdkerrors.Register(ModuleName, 35, "token price proposals should not attempt to update the same denom's price more than once per proposal")
+	ErrTokenPriceMaxBlockAgeMustBePositive                      = sdkerrors.Register(ModuleName, 36, "price max block age must be positive")
+	ErrInvalidPriceMaxBlockAgeParameterType                     = sdkerrors.Register(ModuleName, 37, "price max block age type must be uint64")
 )
