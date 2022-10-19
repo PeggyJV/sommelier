8b76cf4e4a14111b8609568f77a35b5ef70c5a67
Phil <pbal@fordham.edu>
2022-10-19 13:51:54 -0400

    Incorporate first round of comments

--- integration_tests/auction_test.go
+++ integration_tests/auction_test.go
@@ -21,10 +21,21 @@ func (s *IntegrationTestSuite) TestAuctionModule() {
 		s.Require().NoError(err)
 		auctionQueryClient := types.NewQueryClient(val0ClientCtx)
 
-		s.T().Logf("create governance proposal to update some token prices")
+		// Verify auction created for testing exists
+		auctionQuery := types.QueryActiveAuctionRequest{
+			AuctionId: uint32(1),
+		}
+		s.T().Log("Verifying expected auction exists ...")
+		auctionResponse, err := auctionQueryClient.QueryActiveAuction(context.Background(), &auctionQuery)
+		s.Require().NoError(err)
+		s.Require().Equal(auctionResponse.Auction.Id, uint32(1))
+		s.T().Log("Expected auction found!")
+
+		s.T().Logf("Create governance proposal to update some token prices")
 		orch := s.chain.orchestrators[0]
 		orchClientCtx, err := s.chain.clientContext("tcp://localhost:26657", orch.keyring, "orch", orch.keyInfo.GetAddress())
 		s.Require().NoError(err)
+		orch0Address := s.chain.orchestrators[0].keyInfo.GetAddress().String()
 
 		proposal := types.SetTokenPricesProposal{
 			Title:       "initial token price submission",
@@ -55,14 +66,14 @@ func (s *IntegrationTestSuite) TestAuctionModule() {
 			},
 			orch.keyInfo.GetAddress(),
 		)
-		s.Require().NoError(err, "unable to create governance proposal")
+		s.Require().NoError(err, "Unable to create governance proposal")
 
-		s.T().Log("submit proposal")
+		s.T().Log("Submit proposal")
 		submitProposalResponse, err := s.chain.sendMsgs(*orchClientCtx, proposalMsg)
 		s.Require().NoError(err)
 		s.Require().Zero(submitProposalResponse.Code, "raw log: %s", submitProposalResponse.RawLog)
 
-		s.T().Log("check proposal was submitted correctly")
+		s.T().Log("Check proposal was submitted correctly")
 		govQueryClient := govtypes.NewQueryClient(orchClientCtx)
 		proposalsQueryResponse, err := govQueryClient.Proposals(context.Background(), &govtypes.QueryProposalsRequest{})
 		s.Require().NoError(err)
@@ -70,7 +81,7 @@ func (s *IntegrationTestSuite) TestAuctionModule() {
 		s.Require().Equal(uint64(1), proposalsQueryResponse.Proposals[0].ProposalId, "not proposal id 1")
 		s.Require().Equal(govtypes.StatusVotingPeriod, proposalsQueryResponse.Proposals[0].Status, "proposal not in voting period")
 
-		s.T().Log("vote for proposal")
+		s.T().Log("Vote for proposal")
 		for _, val := range s.chain.validators {
 			kr, err := val.keyring()
 			s.Require().NoError(err)
@@ -80,50 +91,43 @@ func (s *IntegrationTestSuite) TestAuctionModule() {
 			voteMsg := govtypes.NewMsgVote(val.keyInfo.GetAddress(), 1, govtypes.OptionYes)
 			voteResponse, err := s.chain.sendMsgs(*localClientCtx, voteMsg)
 			s.Require().NoError(err)
-			s.Require().Zero(voteResponse.Code, "vote error: %s", voteResponse.RawLog)
+			s.Require().Zero(voteResponse.Code, "Vote error: %s", voteResponse.RawLog)
 		}
 
-		s.T().Log("wait for proposal to be approved")
-		s.Require().Eventuallyf(func() bool {
+		s.T().Log("Waiting for proposal to be approved..")
+		s.Require().Eventually(func() bool {
 			proposalQueryResponse, err := govQueryClient.Proposal(context.Background(), &govtypes.QueryProposalRequest{ProposalId: 1})
 			s.Require().NoError(err)
 			return govtypes.StatusPassed == proposalQueryResponse.Proposal.Status
 		}, time.Second*30, time.Second*5, "proposal was never accepted")
-
-		// Verify auction created for testing exists
-		auctionQuery := types.QueryActiveAuctionRequest{
-			AuctionId: uint32(1),
-		}
-		auctionResponse, err := auctionQueryClient.QueryActiveAuction(context.Background(), &auctionQuery)
-		s.Require().NoError(err)
-		s.Require().Equal(auctionResponse.Auction.Id, uint32(1))
-		s.T().Log(auctionResponse.Auction)
+		s.T().Log("Proposal approved!")
 
 		bankQueryClient := banktypes.NewQueryClient(val0ClientCtx)
 		balanceRes, err := bankQueryClient.AllBalances(context.Background(), &banktypes.QueryAllBalancesRequest{Address: authtypes.NewModuleAddress(types.ModuleName).String()})
 		s.Require().NoError(err)
-		s.T().Log(balanceRes)
+		s.T().Logf("Auction module token balances before bids %v", balanceRes.Balances)
 
-		balanceRes, err = bankQueryClient.AllBalances(context.Background(), &banktypes.QueryAllBalancesRequest{Address: s.chain.orchestrators[0].keyInfo.GetAddress().String()})
+		initialOrchBalanceRes, err := bankQueryClient.AllBalances(context.Background(), &banktypes.QueryAllBalancesRequest{Address: orch0Address})
 		s.Require().NoError(err)
-		s.T().Log(balanceRes)
+		s.T().Logf("Orchestrator 0 balances before bids %v", initialOrchBalanceRes.Balances)
 
+		s.T().Log("Submitting first bid for auction")
 		bidRequest1 := types.MsgSubmitBidRequest{
 			AuctionId:              uint32(1),
-			Bidder:                 s.chain.orchestrators[0].keyInfo.GetAddress().String(),
+			Bidder:                 orch0Address,
 			MaxBidInUsomm:          sdk.NewCoin("usomm", sdk.NewIntFromUint64(5000)),
 			SaleTokenMinimumAmount: sdk.NewCoin("gravity0x3506424f91fd33084466f402d5d97f05f8e3b4af", sdk.NewIntFromUint64(1)),
-			Signer:                 s.chain.orchestrators[0].keyInfo.GetAddress().String(),
+			Signer:                 orch0Address,
 		}
 
-		submitBid, err := s.chain.sendMsgs(*orchClientCtx, &bidRequest1)
+		_, err = s.chain.sendMsgs(*orchClientCtx, &bidRequest1)
 		s.Require().NoError(err)
-		s.T().Log(submitBid)
+		s.T().Log("Bid submmitted successfully!")
 
 		auctionResponse, err = auctionQueryClient.QueryActiveAuction(context.Background(), &auctionQuery)
 		s.Require().NoError(err)
-		s.T().Log(auctionResponse.Auction)
 
+		s.T().Log("Verifying auction updated as expected.")
 		// Verify auction updated as expected
 		expectedAuction := types.Auction{
 			Id:                         uint32(1),
@@ -140,20 +144,23 @@ func (s *IntegrationTestSuite) TestAuctionModule() {
 			ProceedsModuleAccount:      cellarfees.ModuleName,
 		}
 		s.Require().Equal(expectedAuction, *auctionResponse.Auction)
+		s.T().Log("Auction updated correctly!")
 
 		// Verify user has funds debited and purchase credited
-		balanceRes, err = bankQueryClient.AllBalances(context.Background(), &banktypes.QueryAllBalancesRequest{Address: s.chain.orchestrators[0].keyInfo.GetAddress().String()})
+		s.T().Log("Verifying user funds debited and credited appropriately.")
+		balanceRes, err = bankQueryClient.AllBalances(context.Background(), &banktypes.QueryAllBalancesRequest{Address: orch0Address})
 		s.Require().NoError(err)
-		s.T().Log(balanceRes)
+		s.T().Logf("Orchestrator 0 token balances after first bid %v", balanceRes.Balances)
 
 		s.Require().Equal(sdk.NewCoin("gravity0x3506424f91fd33084466f402d5d97f05f8e3b4af", sdk.NewInt(2500)), balanceRes.Balances[0])
-		s.Require().Equal(sdk.NewCoin("usomm", sdk.NewInt(99999995000)), balanceRes.Balances[2])
+		s.Require().Equal(sdk.NewCoin("usomm", initialOrchBalanceRes.Balances[1].Amount.Sub(sdk.NewInt(5000))), balanceRes.Balances[2])
+		s.T().Log("User funds updated correctly!")
 
 		// Verify bid is stored as expected
 		expectedBid := types.Bid{
 			Id:                        uint64(1),
 			AuctionId:                 uint32(1),
-			Bidder:                    s.chain.orchestrators[0].keyInfo.GetAddress().String(),
+			Bidder:                    orch0Address,
 			MaxBidInUsomm:             sdk.NewCoin("usomm", sdk.NewIntFromUint64(5000)),
 			SaleTokenMinimumAmount:    sdk.NewCoin("gravity0x3506424f91fd33084466f402d5d97f05f8e3b4af", sdk.NewIntFromUint64(1)),
 			TotalFulfilledSaleTokens:  sdk.NewCoin("gravity0x3506424f91fd33084466f402d5d97f05f8e3b4af", sdk.NewIntFromUint64(2500)),
@@ -161,48 +168,53 @@ func (s *IntegrationTestSuite) TestAuctionModule() {
 			TotalUsommPaid:            sdk.NewCoin("usomm", sdk.NewIntFromUint64(5000)),
 		}
 
+		s.T().Log("Verifying bid stored as expected.")
 		actualBid, err := auctionQueryClient.QueryBid(context.Background(), &types.QueryBidRequest{BidId: uint64(1), AuctionId: uint32(1)})
 		s.Require().NoError(err)
 		s.Require().Equal(expectedBid, *actualBid.Bid)
+		s.T().Log("Bid stored correctly!")
 
+		s.T().Log("Submitting another bid...")
 		// Submit another bid to be partially fulfilled
 		bidRequest2 := types.MsgSubmitBidRequest{
 			AuctionId:              uint32(1),
-			Bidder:                 s.chain.orchestrators[0].keyInfo.GetAddress().String(),
+			Bidder:                 orch0Address,
 			MaxBidInUsomm:          sdk.NewCoin("usomm", sdk.NewIntFromUint64(10000)),
 			SaleTokenMinimumAmount: sdk.NewCoin("gravity0x3506424f91fd33084466f402d5d97f05f8e3b4af", sdk.NewIntFromUint64(50)),
-			Signer:                 s.chain.orchestrators[0].keyInfo.GetAddress().String(),
+			Signer:                 orch0Address,
 		}
 
-		submitBid2, err := s.chain.sendMsgs(*orchClientCtx, &bidRequest2)
+		_, err = s.chain.sendMsgs(*orchClientCtx, &bidRequest2)
 		s.Require().NoError(err)
-		s.T().Log(submitBid2)
+		s.T().Log("Bid submitted successfully!")
 
+		s.T().Log("Verifying user funds debited and credited appropriately.")
 		// Verify user has funds debited and purchase credited
-		balanceRes, err = bankQueryClient.AllBalances(context.Background(), &banktypes.QueryAllBalancesRequest{Address: s.chain.orchestrators[0].keyInfo.GetAddress().String()})
+		balanceRes, err = bankQueryClient.AllBalances(context.Background(), &banktypes.QueryAllBalancesRequest{Address: orch0Address})
 		s.Require().NoError(err)
-		s.T().Log(balanceRes)
+		s.T().Logf("Orchestrator 0 token balances after second bid %v", balanceRes.Balances)
 
 		s.Require().Equal(sdk.NewCoin("gravity0x3506424f91fd33084466f402d5d97f05f8e3b4af", sdk.NewInt(5000)), balanceRes.Balances[0])
-		s.Require().Equal(sdk.NewCoin("usomm", sdk.NewInt(99999990000)), balanceRes.Balances[2])
+		s.Require().Equal(sdk.NewCoin("usomm", initialOrchBalanceRes.Balances[1].Amount.Sub(sdk.NewInt(10000))), balanceRes.Balances[2])
+		s.T().Log("User funds updated correctly!")
 
+		s.T().Log("Verifying auction has completed...")
 		// Verify no active auctions
 		_, err = auctionQueryClient.QueryActiveAuctions(context.Background(), &types.QueryActiveAuctionsRequest{})
 		s.Require().Error(err)
-		s.T().Log(err)
 		s.Require().Equal("rpc error: code = NotFound desc = rpc error: code = NotFound desc = No active auctions found: key not found", err.Error())
+		s.T().Log("Auction completed successfully!")
 
+		s.T().Log("Verifying ended auction stored correctly..")
 		// Verify ended auction
 		endedAuctionResponse, err := auctionQueryClient.QueryEndedAuction(context.Background(), &types.QueryEndedAuctionRequest{AuctionId: uint32(1)})
 		s.Require().NoError(err)
-		s.T().Log(endedAuctionResponse.Auction)
 
 		node, err := orchClientCtx.GetNode()
 		s.Require().NoError(err)
 		status, err := node.Status(context.Background())
 		s.Require().NoError(err)
 		currentBlockHeight := status.SyncInfo.LatestBlockHeight
-		s.T().Log(currentBlockHeight)
 
 		expectedEndedAuction := types.Auction{
 			Id:                         uint32(1),
@@ -219,12 +231,14 @@ func (s *IntegrationTestSuite) TestAuctionModule() {
 			ProceedsModuleAccount:      cellarfees.ModuleName,
 		}
 		s.Require().Equal(expectedEndedAuction, *endedAuctionResponse.Auction)
+		s.T().Log("Ended auction stored correctly!")
 
+		s.T().Log("Verifying expected bid stored correctly...")
 		// Verify bid is stored as expected
 		expectedBid2 := types.Bid{
 			Id:                        uint64(2),
 			AuctionId:                 uint32(1),
-			Bidder:                    s.chain.orchestrators[0].keyInfo.GetAddress().String(),
+			Bidder:                    orch0Address,
 			MaxBidInUsomm:             sdk.NewCoin("usomm", sdk.NewIntFromUint64(10000)),
 			SaleTokenMinimumAmount:    sdk.NewCoin("gravity0x3506424f91fd33084466f402d5d97f05f8e3b4af", sdk.NewIntFromUint64(50)),
 			TotalFulfilledSaleTokens:  sdk.NewCoin("gravity0x3506424f91fd33084466f402d5d97f05f8e3b4af", sdk.NewIntFromUint64(2500)),
@@ -235,5 +249,8 @@ func (s *IntegrationTestSuite) TestAuctionModule() {
 		actualBid2, err := auctionQueryClient.QueryBid(context.Background(), &types.QueryBidRequest{BidId: uint64(2), AuctionId: uint32(1)})
 		s.Require().NoError(err)
 		s.Require().Equal(expectedBid2, *actualBid2.Bid)
+		s.T().Log("Bid stored correctly!")
+
+		s.T().Log("--Test completed successfully--")
 	})
 }
