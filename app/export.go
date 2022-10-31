266fb0764fc235fb21ce9ae78e2a139c9025a7ae
hannydevelop <ukpaiugochiibem@gmail.com>
2022-10-31 13:24:12 +0100

    removed break line in export.go

--- app/export.go
+++ app/export.go
@@ -46,7 +46,6 @@ func (app *SommelierApp) ExportAppStateAndValidators(
 
 // prepare for fresh start at zero height
 // NOTE zero height genesis is a temporary feature which will be deprecated
-//
 //	in favour of export at a block height
 func (app *SommelierApp) prepForZeroHeightGenesis(ctx sdk.Context, jailAllowedAddrs []string) {
 	applyAllowedAddrs := false
