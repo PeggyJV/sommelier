318442dc21f4e9bd582b1bfdeae63900f7c35d89
Eric Bolten <eric@bolten.dev>
2022-12-11 15:20:57 -0800

    add tendermint to grpc gateway

--- app/app.go
+++ app/app.go
@@ -660,6 +660,8 @@ func (app *SommelierApp) RegisterAPIRoutes(apiSvr *api.Server, apiConfig config.
 	authrest.RegisterTxRoutes(clientCtx, apiSvr.Router)
 	// Register new tx routes from grpc-gateway.
 	authtx.RegisterGRPCGatewayRoutes(clientCtx, apiSvr.GRPCGatewayRouter)
+	// Register new tendermint queries routes from grpc-gateway.
+	tmservice.RegisterGRPCGatewayRoutes(clientCtx, apiSvr.GRPCGatewayRouter)
 
 	// Register legacy and grpc-gateway routes for all modules.
 	ModuleBasics.RegisterRESTRoutes(clientCtx, apiSvr.Router)
