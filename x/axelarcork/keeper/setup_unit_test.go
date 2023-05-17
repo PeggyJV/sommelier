b4386129ed2f46b2802d0d8e253870cc8df2f6f9
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2023-05-17 14:40:38 -0700

    add staking mocks

--- x/axelarcork/keeper/setup_unit_test.go
+++ x/axelarcork/keeper/setup_unit_test.go
@@ -1,138 +1,138 @@
 package keeper
 
-//
-//import (
-//	"github.com/peggyjv/sommelier/v6/x/axelarcork/mock/mocks"
-//	"reflect"
-//	"testing"
-//
-//	"github.com/cosmos/cosmos-sdk/codec"
-//	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-//	"github.com/cosmos/cosmos-sdk/store"
-//	storetypes "github.com/cosmos/cosmos-sdk/store/types"
-//	sdk "github.com/cosmos/cosmos-sdk/types"
-//	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
-//
-//	"github.com/golang/mock/gomock"
-//
-//	"github.com/peggyjv/sommelier/v6/x/axelarcork/mock"
-//	"github.com/peggyjv/sommelier/v6/x/axelarcork/types"
-//	"github.com/stretchr/testify/assert"
-//	"github.com/stretchr/testify/require"
-//
-//	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-//	"github.com/tendermint/tendermint/libs/log"
-//	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-//	tmdb "github.com/tendermint/tm-db"
-//)
-//
-//type mocksForCork struct {
-//	mockStakingKeeper *mock.MockStakingKeeper
-//	mockValidator     *mock.MockValidatorI
-//}
-//
-//func setupCorkKeeper(t *testing.T) (
-//	Keeper, sdk.Context, mocksForCork, *gomock.Controller,
-//) {
-//	db := tmdb.NewMemDB()
-//	commitMultiStore := store.NewCommitMultiStore(db)
-//
-//	// Mount the KV store with the x/cork store key
-//	storeKey := sdk.NewKVStoreKey(types.StoreKey)
-//	commitMultiStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
-//
-//	// Mount Transient store
-//	transientStoreKey := sdk.NewTransientStoreKey("transient" + types.StoreKey)
-//	commitMultiStore.MountStoreWithDB(transientStoreKey, sdk.StoreTypeTransient, nil)
-//
-//	// Mount Memory store
-//	memStoreKey := storetypes.NewMemoryStoreKey("mem" + types.StoreKey)
-//	commitMultiStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
-//
-//	require.NoError(t, commitMultiStore.LoadLatestVersion())
-//	protoCodec := codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
-//	params := initParamsKeeper(
-//		protoCodec, codec.NewLegacyAmino(), storeKey, memStoreKey)
-//
-//	subSpace, found := params.GetSubspace(types.ModuleName)
-//	require.True(t, found)
-//
-//	ctrl := gomock.NewController(t)
-//	mockStakingKeeper := mocks.NewMockStakingKeeper(ctrl)
-//
-//	k := NewKeeper(
-//		protoCodec,
-//		storeKey,
-//		subSpace,
-//		mockStakingKeeper,
-//	)
-//
-//	ctx := sdk.NewContext(commitMultiStore, tmproto.Header{}, false, log.NewNopLogger())
-//
-//	return k, ctx, mocksForCork{
-//		mockStakingKeeper: mockStakingKeeper,
-//		mockValidator:     mock.NewMockValidatorI(ctrl),
-//	}, ctrl
-//}
-//
-//func initParamsKeeper(
-//	appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino,
-//	key sdk.StoreKey, tkey sdk.StoreKey,
-//) paramskeeper.Keeper {
-//	paramsKeeper := paramskeeper.NewKeeper(appCodec, legacyAmino, key, tkey)
-//	paramsKeeper.Subspace(types.ModuleName)
-//
-//	return paramsKeeper
-//}
+import (
+	"reflect"
+	"testing"
 
-//func TestSetupCorkKeepers(t *testing.T) {
-//	testCases := []struct {
-//		name string
-//		test func()
-//	}{{
-//		name: "make sure that mocks implement expected keepers interfaces",
-//		test: func() {
-//			k, ctx, mocks, ctrl := setupCorkKeeper(t)
-//			require.PanicsWithError(t, "UnmarshalJSON cannot decode empty bytes",
-//				func() {
-//					params := k.GetParamSet(ctx)
-//					require.NoError(t, params.ValidateBasic())
-//				},
-//			)
-//
-//			for _, keeperPair := range []struct {
-//				expected interface{}
-//				mock     interface{}
-//			}{
-//				{
-//					expected: (*types.StakingKeeper)(nil),
-//					mock:     mocks.mockStakingKeeper,
-//				},
-//				{
-//					expected: (*types.GravityKeeper)(nil),
-//					mock:     mocks.mockGravityKeeper,
-//				},
-//				{
-//					expected: (*stakingtypes.ValidatorI)(nil),
-//					mock:     mocks.mockValidator,
-//				},
-//			} {
-//				_interface := reflect.TypeOf(keeperPair.expected).Elem()
-//				isImplementingExpectedMethods := reflect.
-//					TypeOf(keeperPair.mock).Implements(_interface)
-//				assert.True(t, isImplementingExpectedMethods)
-//			}
-//
-//			defer ctrl.Finish()
-//		},
-//	},
-//	}
-//
-//	for _, tc := range testCases {
-//		tc := tc
-//		t.Run(tc.name, func(t *testing.T) {
-//			tc.test()
-//		})
-//	}
-//
-//}
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/store"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
+
+	"github.com/golang/mock/gomock"
+
+	mock_types "github.com/peggyjv/sommelier/v6/x/axelarcork/tests"
+	"github.com/peggyjv/sommelier/v6/x/axelarcork/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmdb "github.com/tendermint/tm-db"
+)
+
+type mocksForCork struct {
+	mockStakingKeeper *mock_types.MockStakingKeeper
+	mockValidator     *mock_types.MockValidatorI
+}
+
+func setupCorkKeeper(t *testing.T) (
+	Keeper, sdk.Context, mocksForCork, *gomock.Controller,
+) {
+	db := tmdb.NewMemDB()
+	commitMultiStore := store.NewCommitMultiStore(db)
+
+	// Mount the KV store with the x/cork store key
+	storeKey := sdk.NewKVStoreKey(types.StoreKey)
+	commitMultiStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
+
+	// Mount Transient store
+	transientStoreKey := sdk.NewTransientStoreKey("transient" + types.StoreKey)
+	commitMultiStore.MountStoreWithDB(transientStoreKey, sdk.StoreTypeTransient, nil)
+
+	// Mount Memory store
+	memStoreKey := storetypes.NewMemoryStoreKey("mem" + types.StoreKey)
+	commitMultiStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
+
+	require.NoError(t, commitMultiStore.LoadLatestVersion())
+	protoCodec := codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+	params := initParamsKeeper(
+		protoCodec, codec.NewLegacyAmino(), storeKey, memStoreKey)
+
+	subSpace, found := params.GetSubspace(types.ModuleName)
+	require.True(t, found)
+
+	ctrl := gomock.NewController(t)
+	mockStakingKeeper := mock_types.NewMockStakingKeeper(ctrl)
+	mockTransferKeeper := mock_types.NewMockTransferKeeper(ctrl)
+	mockDistributionKeeper := mock_types.NewMockDistributionKeeper(ctrl)
+	mockICS4wrapper := mock_types.NewMockICS4Wrapper(ctrl)
+
+	k := NewKeeper(
+		protoCodec,
+		storeKey,
+		subSpace,
+		mockStakingKeeper,
+		mockTransferKeeper,
+		mockDistributionKeeper,
+		mockICS4wrapper,
+	)
+
+	ctx := sdk.NewContext(commitMultiStore, tmproto.Header{}, false, log.NewNopLogger())
+
+	return k, ctx, mocksForCork{
+		mockStakingKeeper: mockStakingKeeper,
+		mockValidator:     mock_types.NewMockValidatorI(ctrl),
+	}, ctrl
+}
+
+func initParamsKeeper(
+	appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino,
+	key sdk.StoreKey, tkey sdk.StoreKey,
+) paramskeeper.Keeper {
+	paramsKeeper := paramskeeper.NewKeeper(appCodec, legacyAmino, key, tkey)
+	paramsKeeper.Subspace(types.ModuleName)
+
+	return paramsKeeper
+}
+
+func TestSetupCorkKeepers(t *testing.T) {
+	testCases := []struct {
+		name string
+		test func()
+	}{{
+		name: "make sure that mocks implement expected keepers interfaces",
+		test: func() {
+			k, ctx, mocks, ctrl := setupCorkKeeper(t)
+			require.PanicsWithError(t, "UnmarshalJSON cannot decode empty bytes",
+				func() {
+					params := k.GetParamSet(ctx)
+					require.NoError(t, params.ValidateBasic())
+				},
+			)
+
+			for _, keeperPair := range []struct {
+				expected interface{}
+				mock     interface{}
+			}{
+				{
+					expected: (*types.StakingKeeper)(nil),
+					mock:     mocks.mockStakingKeeper,
+				},
+				{
+					expected: (*stakingtypes.ValidatorI)(nil),
+					mock:     mocks.mockValidator,
+				},
+			} {
+				_interface := reflect.TypeOf(keeperPair.expected).Elem()
+				isImplementingExpectedMethods := reflect.
+					TypeOf(keeperPair.mock).Implements(_interface)
+				assert.True(t, isImplementingExpectedMethods)
+			}
+
+			defer ctrl.Finish()
+		},
+	},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			tc.test()
+		})
+	}
+
+}
