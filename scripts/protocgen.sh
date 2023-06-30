c0f091ac7f26221f469ef082b057e320ae818169
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2023-06-30 11:34:30 -0700

    Bolten/test axelar buf (#220)
    * old protocgen.sh
    
    * newline fix
    
    * regen protos
    
    ---------
    
    Co-authored-by: Eric Bolten <eric@bolten.dev>

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,19 +2,44 @@
 
 set -eo pipefail
 
-echo "Generating gogo proto code"
-cd proto
-proto_dirs=$(find  -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+protoc_gen_gocosmos() {
+  if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
+    echo -e "\tPlease run this command from somewhere inside the sommelier folder."
+    return 1
+  fi
+
+  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@latest 2>/dev/null
+}
+
+protoc_gen_gocosmos
+
+echo "generating proto and gRPC gateway files..."
+
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep "option go_package" $file &> /dev/null ; then
-      buf generate --template buf.gen.gogo.yml $file
-    fi
-  done
+  buf protoc \
+  -I "proto" \
+  -I "third_party/proto" \
+  --gocosmos_out=plugins=interfacetype+grpc,\
+Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
+  --grpc-gateway_out=logtostderr=true,allow_colon_final_segments=true:. \
+  $(find "${dir}" -maxdepth 1 -name '*.proto')
+
 done
 
-cd ..
+echo "generating proto docs..."
+# command to generate docs using protoc-gen-doc
+buf protoc \
+-I "proto" \
+-I "third_party/proto" \
+--doc_out=./docs/core \
+--doc_opt=./docs/protodoc-markdown.tmpl,proto-docs.md \
+$(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
+go mod tidy
 
+echo "(re)moving proto files.."
 # move proto files to the right places
 cp -r github.com/peggyjv/sommelier/* ./
 rm -rf github.com
+
+echo "done"
