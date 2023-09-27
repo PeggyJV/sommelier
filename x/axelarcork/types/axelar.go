9aadba321aef8a4b7e8caa35d2ce0220fa0d694c
Collin Brittain <collinbrittain@gmail.com>
2023-09-27 16:31:03 -0500

    Remove unused struct

--- x/axelarcork/types/axelar.go
+++ x/axelarcork/types/axelar.go
@@ -1,7 +1,5 @@
 package types
 
-import "math/big"
-
 const (
 	_ int64 = iota
 	PureMessage
@@ -15,13 +13,6 @@ type AxelarBody struct {
 	Payload            []byte `json:"payload"`
 	Type               int64  `json:"type"`
 	Fee                *Fee   `json:"fee"`
-}
-
-type ProxyWrapper struct {
-	Target   string   `json:"target"`
-	Nonce    *big.Int `json:"nonce"`
-	Deadline *big.Int `json:"deadline"`
-	Body     []byte   `json:"body"`
 }
 
 type Fee struct {
