c62622e91632a693a76c37cfa1c185b63f76c8b4
Collin Brittain <collinbrittain@gmail.com>
2024-03-15 12:53:31 -0500

    CLI queries and tests

--- x/addresses/client/cli/query.go
+++ x/addresses/client/cli/query.go
@@ -3,12 +3,14 @@ package cli
 import (
 	"context"
 	"fmt"
+
 	// "strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+
 	// "github.com/cosmos/cosmos-sdk/client/flags"
 	// sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -27,7 +29,9 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(CmdQueryParams())
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(CmdQueryAddressMappings())
+	cmd.AddCommand(CmdQueryAddressMappingByCosmosAddress())
+	cmd.AddCommand(CmdQueryAddressMappingByEVMAddress())
 
 	return cmd
 }
@@ -43,6 +47,81 @@ func CmdQueryParams() *cobra.Command {
 			queryClient := types.NewQueryClient(clientCtx)
 
 			res, err := queryClient.QueryParams(context.Background(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdQueryAddressMappings() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "address-mappings",
+		Aliases: []string{"am"},
+		Short:   "shows the address mappings",
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.QueryAddressMappings(context.Background(), &types.QueryAddressMappingsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdQueryAddressMappingByCosmosAddress() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "address-mapping-by-cosmos-address [cosmos-address]",
+		Aliases: []string{"ambca"},
+		Short:   "shows evm address that maps to the provided cosmos address",
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.QueryAddressMappingByCosmosAddress(context.Background(), &types.QueryAddressMappingByCosmosAddressRequest{CosmosAddress: args[0]})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdQueryAddressMappingByEVMAddress() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "address-mapping-by-evm-address [evm-address]",
+		Aliases: []string{"ambea"},
+		Short:   "shows cosmos address that maps to the provided evm address",
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.QueryAddressMappingByEVMAddress(context.Background(), &types.QueryAddressMappingByEVMAddressRequest{EvmAddress: args[0]})
 			if err != nil {
 				return err
 			}
