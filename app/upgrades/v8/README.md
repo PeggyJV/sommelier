f690389f89505da541ddaed94185c3ea977556ee
Collin Brittain <collinbrittain@gmail.com>
2024-10-09 13:16:14 -0500

    Minor tweak to upgrade handler

--- app/upgrades/v8/README.md
+++ app/upgrades/v8/README.md
@@ -4,5 +4,7 @@ This upgrade moves Sommelier to major version 8.
 
 ## Summary of changes
 
+* Add the addresses module for mapping cosmos/evm addresses
 * Update the cellarfees module to start fee auctions based on the accrued USD value of a particular denom
 * Update the auction module to allow a portion of SOMM proceeds earned by auctions to be burned
+* Upgrade the gravity module to v5
--- app/upgrades/v8/upgrades.go
+++ app/upgrades/v8/upgrades.go
@@ -11,13 +11,8 @@ func CreateUpgradeHandler(
 	configurator module.Configurator,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		ctx.Logger().Info("v2 upgrade: entering handler and running migrations")
+		ctx.Logger().Info("v8 upgrade: entering handler and running migrations")
 
-		fromVM := make(map[string]uint64)
-		for moduleName, m := range mm.Modules {
-			fromVM[moduleName] = m.(module.HasConsensusVersion).ConsensusVersion()
-		}
-
-		return mm.RunMigrations(ctx, configurator, fromVM)
+		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
