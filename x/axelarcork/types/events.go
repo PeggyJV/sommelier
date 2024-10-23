e6969d7f0f5b50faa56113b9fb92b8bb058ff82c
Collin <collinbrittain@gmail.com>
2024-10-22 19:46:44 -0500

    Additional cork and axelarcork events for easier tracking (#322)
    * More cork events
    
    * Axelarcork events
    
    * Fix linting errors

---
+++ x/axelarcork/types/events.go
@@ -0,0 +1,12 @@
+package types
+
+// axelarcork module event types
+const (
+	EventTypeAxelarCorkApproved = "axelar_cork_approved"
+
+	AttributeKeyCork        = "cork"
+	AttributeKeyBlockHeight = "block_height"
+	AttributeKeyCorkID      = "cork_id"
+
+	AttributeValueCategory = ModuleName
+)
