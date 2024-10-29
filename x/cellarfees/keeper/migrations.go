84fcf6573cccb41acb64aa9b2b8d4fb77195de28
Collin Brittain <collinbrittain@gmail.com>
2024-10-29 13:47:08 -0500

    Hopefully last cellarfees params fix

--- x/cellarfees/keeper/migrations.go
+++ x/cellarfees/keeper/migrations.go
@@ -27,7 +27,7 @@ func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 		return err
 	}
 
-	m.keeper.paramSpace.WithKeyTable(v2types.ParamKeyTable()).SetParamSet(ctx, newParams)
+	m.keeper.paramSpace.SetParamSet(ctx, newParams)
 
 	ctx.Logger().Info("cellarfees v1 to v2: Params migration complete")
 
